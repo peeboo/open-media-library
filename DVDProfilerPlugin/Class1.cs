319236d2d108406d906b7d982f06e5b563a928e8
DJShultz <DJShultz@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2008-04-29 18:06:23 +0000

    Copied the Title object into the SDK so that we could remove the dependency on the OMLEngine for plugin development.  I'll finish up the OMLEngine integration of finding/loading/using plugins (via reflection) so that I can then finish off the validate plugin method and start finding/loading the plugins into the editor app

--- DVDProfilerPlugin/Class1.cs
+++ DVDProfilerPlugin/Class1.cs
@@ -6,10 +6,5 @@ namespace DVDProfilerPlugin
 {
     public class Class1 : OMLPlugin
     {
-        public OMLDataSet GetOmlDataSet()
-        {
-            OMLDataSet ods = new OMLDataSet();
-            return ods;
-        }
     }
 }
