f1de0605ab6dfafe9dd2fcb1d24fe630f292f2ef
DJShultz <DJShultz@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2008-04-29 05:30:46 +0000

    Beginnings of a working MovieCollectorz plugin is now up.  ALOT needs to be fixed (such as figuring out how to remove the OMLEngine reference requirement.  But atleast this way we can add titles (via this plugin).

--- DVDProfilerPlugin/Class1.cs
+++ DVDProfilerPlugin/Class1.cs
@@ -4,7 +4,7 @@ using OMLSDK;
 
 namespace DVDProfilerPlugin
 {
-    public class Class1 : IDataImporter
+    public class Class1 : OMLPlugin
     {
         public OMLDataSet GetOmlDataSet()
         {
--- OMLSDK/OMLPlugin.cs
+++ OMLSDK/OMLPlugin.cs
@@ -1,21 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using OMLEngine;
 
 namespace OMLSDK
 {
     public class OMLPlugin
     {
-        public static bool ValidateRow(OMLDataSet ds, DataRow row)
+        List<Title> titles;
+        private int totalRowsAdded = 0;
+
+        public int TotalRowsAdded
         {
+            get { return totalRowsAdded; }
+        }
 
-            foreach (string columnname in ds.GetColumnNames())
-            {
-                if (row[columnname] == null)
-                    return false;
-            }
-
+        public OMLPlugin()
+        {
+            titles = new List<Title>();
+        }
+        public Title newTitle()
+        {
+            return new Title();
+        }
+        public void AddTitle(Title newTitle)
+        {
+            titles.Add(newTitle);
+            totalRowsAdded++;
+        }
+        public bool ValidateTitle(Title title_to_validate)
+        {
             return true;
+        }
+        public void CompleteAdditions()
+        {
+            TitleCollection tc = new TitleCollection();
+            tc.loadTitleCollection();
+            foreach (Title newTitle in titles)
+            {
+                tc.Add(newTitle);
+            }
+            tc.saveTitleCollection();
+        }
+        public string getName()
+        {
+            throw new Exception("Called base class getName()");
         }
     }
 }
