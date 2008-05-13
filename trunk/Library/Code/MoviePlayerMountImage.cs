73bc1fd710b072ef81d1764d9eb0a386b4979cd5
taranu <taranu@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2008-05-13 04:15:55 +0000

    Split the players into different files

---
+++ trunk/Library/Code/MoviePlayerMountImage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OMLEngine;
+using Microsoft.MediaCenter.Hosting;
+using Microsoft.MediaCenter;
+using Microsoft.MediaCenter.UI;
+using System.IO;
+
+namespace Library
+{
+    public class MountImagePlayer : IPlayMovie
+    {
+        public MountImagePlayer(MovieItem title)
+        {
+            _title = title;
+        }
+
+        public bool PlayMovie()
+        {
+            AddInHost.Current.MediaCenterEnvironment.Dialog("Mounting images not implemented yet. File " + _title.FileLocation, "Error", DialogButtons.Cancel, 0, true);
+
+            // mount the file, then figure out which other player we want to create
+            return false;
+        }
+        MovieItem _title;
+    }
+
+
+}
