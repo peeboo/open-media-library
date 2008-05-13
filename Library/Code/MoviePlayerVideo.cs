d292101755ed0741f3a289490db6e953e8b08968
taranu <taranu@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2008-05-13 04:15:55 +0000

    Split the players into different files

---
+++ Library/Code/MoviePlayerVideo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OMLEngine;
+using Microsoft.MediaCenter.Hosting;
+using Microsoft.MediaCenter;
+using Microsoft.MediaCenter.UI;
+using System.IO;
+
+namespace Library
+{
+    /// <summary>
+    /// VideoPlayer class for playing standard videos (AVIs, etc)
+    /// </summary>
+    public class VideoPlayer : IPlayMovie
+    {
+        public VideoPlayer(MovieItem title)
+        {
+            _title = title;
+        }
+
+        public bool PlayMovie()
+        {
+            if (AddInHost.Current.MediaCenterEnvironment.PlayMedia(MediaType.Video, _title.FileLocation, false))
+            {
+                if (AddInHost.Current.MediaCenterEnvironment.MediaExperience != null)
+                {
+                    AddInHost.Current.MediaCenterEnvironment.MediaExperience.GoToFullScreen();
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        MovieItem _title;
+    }
+}
