add87f7b9ec5d7b4271778487635fbe9d399841e
DJShultz <DJShultz@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2008-05-16 06:17:06 +0000

    Writing more tests, found some small bugs while testing, fixed them and all tests pass. We really need to write ALOT more tests

---
+++ trunk/OMLTestSuite/OMLConfigManagerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using OMLEngine;
+using NUnit.Framework;
+
+namespace OMLTestSuite
+{
+    [TestFixture]
+    public class OMLConfigManagerTest
+    {
+        [TearDown]
+        public void TearDownMethod()
+        {
+        }
+
+        [Test]
+        public void TEST_BASE_CASE()
+        {
+            OMLConfigManager cm = new OMLConfigManager();
+        }
+    }
+}
--- trunk/OMLTestSuite/Program.cs
+++ trunk/OMLTestSuite/Program.cs
@@ -9,8 +9,20 @@ namespace OMLTestSuite
     {
         static void Main(string[] args)
         {
-            MyMoviesPluginTest test = new MyMoviesPluginTest();
-            test.TEST_BASE_CASE();
+            MyMoviesPluginTest mmpt = new MyMoviesPluginTest();
+            mmpt.TEST_BASE_CASE();
+
+            TitleTest tt = new TitleTest();
+            tt.TEST_BASE_CASE();
+
+            TitleCollectionTest tct = new TitleCollectionTest();
+            tct.TEST_BASE_CASE();
+            tct.TEST_FIND_FOR_ID();
+            tct.TEST_SOURCE_DATABASE_TO_USE();
+            tct.TEST_TO_DATASET_TABLE_OBJECT();
+
+            OMLConfigManagerTest omlcm = new OMLConfigManagerTest();
+            omlcm.TEST_BASE_CASE();
         }
     }
 }
