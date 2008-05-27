75412d2ef91b0b1cd12646486b63bfbe29187a08
DJShultz <DJShultz@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2008-05-27 05:38:02 +0000

    TranscodePlayer is now framed out.  I figured out how to use Weak referenced types with Remoting (not that bad really).  I just need to flesh out the playing feature and the closing out the tcpclientchannel when the video is complete.

--- trunk/Library/Code/MoviePlayerTranscode.cs
+++ trunk/Library/Code/MoviePlayerTranscode.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Collections;
+using System.Reflection;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
 using OMLEngine;
 using Microsoft.MediaCenter.Hosting;
 using Microsoft.MediaCenter;
@@ -18,54 +21,118 @@ namespace Library
 
         public bool PlayMovie()
         {
+            string path_to_buffer = "\\" + _title.itemId;
+            return PlayTranscodedMedia(ref path_to_buffer);
+            /*
             // mount the file, then figure out which other player we want to create
             AddInHost.Current.MediaCenterEnvironment.Dialog("Transcoding not implemented yet. File " + _title.FileLocation, "Error", DialogButtons.Cancel, 0, true);
             return false;
+            */
         }
         MovieItem _title;
 
         public bool PlayTranscodedMedia(ref string path_to_buffer)
         {
-            return false;
-            /*
             Type ITranscode360Type = null;
+            MethodInfo MIIsMediaTranscodeComplete = null;
+            MethodInfo MIIsMediaTranscoding = null;
+            MethodInfo MIIsMediaTranscodingWithParams = null;
+            MethodInfo MITranscode = null;
+            MethodInfo MITranscodeWithParams = null;
+            MethodInfo MIStopTranscoding = null;
+
             if (Utilities.IsTranscode360LibraryAvailable())
             {
                 ITranscode360Type =
                     Utilities.LoadTranscode360Assembly(
                         @"c:\\program files\\transcode360\\Transcode360.Interface.dll");
+
                 if (ITranscode360Type != null)
                 {
-                    try
+                    Hashtable properties = new Hashtable();
+                    properties.Add("name", "");
+                    TcpClientChannel channel = new TcpClientChannel(properties, null);
+                    ChannelServices.RegisterChannel(channel);
+
+                    object server = Activator.GetObject(ITranscode360Type,
+                        "tcp://localhost:1401/RemotingServices/Transcode360");
+
+                    assignMethodInfoObjects(ITranscode360Type,
+                                            ref MIIsMediaTranscodeComplete,
+                                            ref MIIsMediaTranscoding,
+                                            ref MIIsMediaTranscodingWithParams,
+                                            ref MITranscode,
+                                            ref MITranscodeWithParams,
+                                            ref MIStopTranscoding);
+
+
+                    if (MIIsMediaTranscodeComplete != null &&
+                        MIIsMediaTranscoding != null &&
+                        MIIsMediaTranscodingWithParams != null &&
+                        MITranscode != null &&
+                        MITranscodeWithParams != null &&
+                        MIStopTranscoding != null)
                     {
-                        Hashtable properties = new Hashtable();
-                        properties.Add("name", "");
-                        TcpClientChannel channel = new TcpClientChannel(properties, null);
-                        ChannelServices.RegisterChannel(channel);
-
-                        object obj = Activator.GetObject(typeof(ITranscode360Type),
-                                                         "tcp://localhost:1401/RemotingServices/Transcode360");
-
-                        // call an interface, not interested in the result as long as we don't get a 
-                        // socket/remoting exception we're happy
-                        if (obj.Transcode(FileLocation, out path_to_buffer, DateTime.Now.ToBinary()))
-                        {
-                            if (obj.IsMediaTranscoding(FileLocation))
-                                return true;
-                        }
-
-                        ChannelServices.UnregisterChannel(channel);
-                    }
-                    catch (Exception ex)
-                    {
-                        Trace.WriteLine(ex.Message);
+                        object[] paramArray = new object[1];
+                        paramArray[0] = _title.FileLocation;
+                        MITranscode.Invoke(server, paramArray);
                     }
                 }
             }
             return false;
-            */
         }
 
+        private void assignMethodInfoObjects(Type ITranscode360Type,
+                                             ref MethodInfo MIIsMediaTranscodeComplete,
+                                             ref MethodInfo MIIsMediaTranscoding,
+                                             ref MethodInfo MIIsMediaTranscodingWithParams,
+                                             ref MethodInfo MITranscode,
+                                             ref MethodInfo MITranscodeWithParams,
+                                             ref MethodInfo MIStopTranscoding)
+        {
+                    MethodInfo[] methodInfos = ITranscode360Type.GetMethods();
+
+                    foreach (MethodInfo mi in methodInfos)
+                    {
+                        switch (mi.Name)
+                        {
+                            case "IsMediaTranscodeComplete":
+                                MIIsMediaTranscodeComplete = mi;
+                                break;
+                            case "IsMediaTranscoding":
+                                switch (mi.GetParameters().Length)
+                                {
+                                    case 1:
+                                        MIIsMediaTranscoding = mi;
+                                        break;
+                                    case 3:
+                                        MIIsMediaTranscodingWithParams = mi;
+                                        break;
+                                    default:
+                                        break;
+                                }
+                                break;
+                            case "Transcode":
+                                switch (mi.GetParameters().Length)
+                                {
+                                    case 3:
+                                        MITranscode = mi;
+                                        break;
+                                    case 6:
+                                        MITranscodeWithParams = mi;
+                                        break;
+                                    default:
+                                        break;
+                                }
+                                break;
+                            case "StopTranscoding":
+                                MIStopTranscoding = mi;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+        }
 
         private string DynamicPlayMedia()
         {
