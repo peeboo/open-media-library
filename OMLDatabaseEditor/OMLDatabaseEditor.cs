a23f06a4bd9aa5af99135f9202c1bea4bf140a57
carazy <carazy@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2008-05-30 07:46:49 +0000

    DB editor will now load titles of library.

--- OMLDatabaseEditor/OMLDatabaseEditor.cs
+++ OMLDatabaseEditor/OMLDatabaseEditor.cs
@@ -23,10 +23,11 @@ namespace OMLDatabaseEditor
             InitializeComponent();
             _titleCollection = new TitleCollection();
             _titleCollection.loadTitleCollection();
+            formatDG();
             SetupTitleList();
-            SetupRating();
-            current_title = new Title();
-            current_title_has_changed = false;
+            //SetupRating();
+            //current_title = new Title();
+            //current_title_has_changed = false;
         }
 
         private void SetupRating()
@@ -41,140 +42,143 @@ namespace OMLDatabaseEditor
 
         private void SetupTitleList()
         {
-            dt = GetTitlesDataTable();
+            dt = _titleCollection.ToDataTable();
+
             dgv_title_list.AutoGenerateColumns = false;
             dgv_title_list.DataSource = dt;
         }
-        private DataTable GetTitlesDataTable()
-        {
-            DataTable dt = new DataTable("Titles");
-            DataColumn idCol = new DataColumn("itemId");
-            idCol.DataType = typeof(int);
-            dt.Columns.Add(idCol);
+        //private DataTable GetTitlesDataTable()
+        //{
+        //    DataTable dt = new DataTable("Titles");
+        //    DataColumn idCol = new DataColumn("itemId");
+        //    idCol.DataType = typeof(int);
+        //    dt.Columns.Add(idCol);
 
-            DataColumn nameCol = new DataColumn("TitleName");
-            nameCol.DataType = typeof(string);
-            dt.Columns.Add(nameCol);
+        //    DataColumn nameCol = new DataColumn("TitleName");
+        //    nameCol.DataType = typeof(string);
+        //    dt.Columns.Add(nameCol);
 
-            DataColumn[] PrimaryKeyColumns = new DataColumn[1];
-            PrimaryKeyColumns[0] = dt.Columns["itemId"];
-            dt.PrimaryKey = PrimaryKeyColumns;
+        //    DataColumn[] PrimaryKeyColumns = new DataColumn[1];
+        //    PrimaryKeyColumns[0] = dt.Columns["itemId"];
+        //    dt.PrimaryKey = PrimaryKeyColumns;
 
-            DataRow row;
-            foreach (Title title in _titleCollection)
-            {
-                row = dt.NewRow();
-                row["itemId"] = title.InternalItemID;
-                row["TitleName"] = title.Name;
-                dt.Rows.Add(row);
-            }
+        //    DataRow row;
+        //    foreach (Title title in _titleCollection)
+        //    {
+        //        row = dt.NewRow();
+        //        row["itemId"] = title.InternalItemID;
+        //        row["TitleName"] = title.Name;
+        //        dt.Rows.Add(row);
+        //    }
 
-            return dt;
-        }
-        private void toolStripButton1_Click(object sender, EventArgs e)
-        {
-        }
-        private void bindingNavigatorDeleteItem_Click(object sender, EventArgs e)
-        {
-            _titleCollection.Remove(current_title);
-        }
+        //    return dt;
+        //}
+        //private void toolStripButton1_Click(object sender, EventArgs e)
+        //{
+        //}
+        //private void bindingNavigatorDeleteItem_Click(object sender, EventArgs e)
+        //{
+        //    _titleCollection.Remove(current_title);
+        //}
         private void dgv_title_list_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
-            DataRow row = dt.Rows[e.RowIndex];
+        //    DataRow row = dt.Rows[e.RowIndex];
 
-            foreach (Title t in _titleCollection)
-            {
-                if (t.InternalItemID.CompareTo((int)row["itemId"]) == 0)
-                    if (CheckAndUpdateCurrentTitle())
-                        SetNewCurrentTitle(t);
-            }
+        //    foreach (Title t in _titleCollection)
+        //    {
+        //        if (t.InternalItemID.CompareTo((int)row["itemId"]) == 0)
+        //            if (CheckAndUpdateCurrentTitle())
+        //                SetNewCurrentTitle(t);
+        //    }
         }
-        private bool CheckAndUpdateCurrentTitle()
-        {
-            if (current_title != null)
-            {
-                if (current_title_has_changed)
-                {
-                    DialogResult result = MessageBox.Show(
-                        "The current title seems to have some changes, would you like to save them?",
-                        "Save Changes", MessageBoxButtons.YesNo);
+        //private bool CheckAndUpdateCurrentTitle()
+        //{
+        //    if (current_title != null)
+        //    {
+        //        if (current_title_has_changed)
+        //        {
+        //            DialogResult result = MessageBox.Show(
+        //                "The current title seems to have some changes, would you like to save them?",
+        //                "Save Changes", MessageBoxButtons.YesNo);
 
-                    switch (result)
-                    {
-                        case DialogResult.Yes:
-                            SaveCurrentTitle();
-                            break;
-                        case DialogResult.No:
-                            break;
-                    }
-                }
-            }
-            return true;
-        }
-        private void SaveCurrentTitle()
-        {
-            current_title.Name = tbTitle.Text;
-            current_title.Description = tbDescription.Text;
-            current_title.ReleaseDate = dtpReleaseDate.Value;
-            current_title.Runtime = Int32.Parse(tbRunTime.Text);
-            //current_title.MPAARating = cbRating.Text;
-            current_title.Distributor = tbDistributor.Text;
-            current_title.CountryOfOrigin = tbCountryOfOrigin.Text;
-            current_title.OfficialWebsiteURL = tbWebsite.Text;
-            current_title.ImporterSource = tbImporterSource.Text;
-            current_title.DateAdded = dtpDateAdded.Value;
-            current_title.Synopsis = tbSynopsis.Text;
+        //            switch (result)
+        //            {
+        //                case DialogResult.Yes:
+        //                    SaveCurrentTitle();
+        //                    break;
+        //                case DialogResult.No:
+        //                    break;
+        //            }
+        //        }
+        //    }
+        //    return true;
+        //}
+        //private void SaveCurrentTitle()
+        //{
+        //    current_title.Name = tbTitle.Text;
+        //    current_title.Description = tbDescription.Text;
+        //    current_title.ReleaseDate = dtpReleaseDate.Value;
+        //    current_title.Runtime = Int32.Parse(tbRunTime.Text);
+        //    //current_title.MPAARating = cbRating.Text;
+        //    current_title.Distributor = tbDistributor.Text;
+        //    current_title.CountryOfOrigin = tbCountryOfOrigin.Text;
+        //    current_title.OfficialWebsiteURL = tbWebsite.Text;
+        //    current_title.ImporterSource = tbImporterSource.Text;
+        //    current_title.DateAdded = dtpDateAdded.Value;
+        //    current_title.Synopsis = tbSynopsis.Text;
 
-            _titleCollection.saveTitleCollection();
-        }
-        private void SetNewCurrentTitle(Title t)
-        {
-            current_title = t;
-            tbTitle.Text = current_title.Name;
-            tbDescription.Text = current_title.Description;
-            if (current_title.ReleaseDate.Year > 0001)
-                dtpReleaseDate.Value = current_title.ReleaseDate;
+        //    _titleCollection.saveTitleCollection();
+        //}
+        //private void SetNewCurrentTitle(Title t)
+        //{
+        //    current_title = t;
+        //    tbTitle.Text = current_title.Name;
+        //    tbDescription.Text = current_title.Description;
+        //    if (current_title.ReleaseDate.Year > 0001)
+        //        dtpReleaseDate.Value = current_title.ReleaseDate;
 
-            tbRunTime.Text = current_title.Runtime.ToString();
-            //cbRating.SelectedIndex = (int)current_title.MPAARating;
-            tbDistributor.Text = current_title.Distributor;
-            tbCountryOfOrigin.Text = current_title.CountryOfOrigin;
-            tbWebsite.Text = current_title.OfficialWebsiteURL;
-            tbImporterSource.Text = current_title.ImporterSource;
-            //dtpDateAdded.Value = current_title.DateAdded;
-            tbSynopsis.Text = current_title.Synopsis;
+        //    tbRunTime.Text = current_title.Runtime.ToString();
+        //    //cbRating.SelectedIndex = (int)current_title.MPAARating;
+        //    tbDistributor.Text = current_title.Distributor;
+        //    tbCountryOfOrigin.Text = current_title.CountryOfOrigin;
+        //    tbWebsite.Text = current_title.OfficialWebsiteURL;
+        //    tbImporterSource.Text = current_title.ImporterSource;
+        //    //dtpDateAdded.Value = current_title.DateAdded;
+        //    tbSynopsis.Text = current_title.Synopsis;
 
-            if (current_title.FrontCoverPath != null && current_title.FrontCoverPath.Length > 0)
-            {
-                try
-                {
-                    front_cover = new Bitmap(current_title.FrontCoverPath);
-                    pbFrontCover.Image = (Image)front_cover;
-                }
-                catch (Exception)
-                {
-                }
-            }
-            else
-            {
-                pbFrontCover.Image = pbFrontCover.InitialImage;
-            }
+        //    if (current_title.FrontCoverPath != null && current_title.FrontCoverPath.Length > 0)
+        //    {
+        //        try
+        //        {
+        //            front_cover = new Bitmap(current_title.FrontCoverPath);
+        //            pbFrontCover.Image = (Image)front_cover;
+        //        }
+        //        catch (Exception)
+        //        {
+        //        }
+        //    }
+        //    else
+        //    {
+        //        pbFrontCover.Image = pbFrontCover.InitialImage;
+        //    }
 
-            if (current_title.BackCoverPath != null && current_title.BackCoverPath.Length > 0)
-            {
-                back_cover = new Bitmap(current_title.BackCoverPath);
-                pbBackCover.Image = (Image)back_cover;
-            }
-            else
-            {
-                pbBackCover.Image = pbBackCover.InitialImage;
-            }
-        }
+        //    if (current_title.BackCoverPath != null && current_title.BackCoverPath.Length > 0)
+        //    {
+        //        back_cover = new Bitmap(current_title.BackCoverPath);
+        //        pbBackCover.Image = (Image)back_cover;
+        //    }
+        //    else
+        //    {
+        //        pbBackCover.Image = pbBackCover.InitialImage;
+        //    }
+        //}
 
         private void button1_Click(object sender, EventArgs e)
         {
-            current_title_has_changed = true;
+        //    current_title_has_changed = true;
         }
+
+        
         /*
         private void OMLDatabaseEditor_DragDrop(object sender, DragEventArgs e)
         {
@@ -186,5 +190,52 @@ namespace OMLDatabaseEditor
             MessageBox.Show("Boo!");
         }
         */
+
+        private void dgv_title_list_cellFormatting(object sender, DataGridViewCellFormattingEventArgs e) 
+        {
+        //    if (dgv_title_list.Columns[e.ColumnIndex].Name == "Cover")
+        //    {
+        //        //if (dgv_title_list.Rows[e.RowIndex].Cells["Cover"].Value.ToString() != "")
+        //        //{
+        //            string fileName = (string)dgv_title_list.Rows[e.RowIndex].Cells["FrontCoverPath"].Value.ToString(); 
+        //                //fileName = dgv_title_list.Rows[e.RowIndex].Cells["Cover"].Value.ToString;
+        //            if (System.IO.File.Exists(fileName))
+        //            {
+        //                e.Value = Image.FromFile(fileName);
+        //            }
+        //        //}
+        //    }
+        }
+            
+        private void formatDG()
+        {
+            foreach (DataGridViewColumn c in dgv_title_list.Columns)
+            {
+                switch (c.Name)
+                {
+                    case "id":
+                        c.DataPropertyName = "id";
+                        c.Visible = false;
+                        break;
+
+                    case "FrontCoverPath":
+                        c.DataPropertyName = "FrontCoverPath";
+                        c.Visible = false;
+                        break;
+                    
+                    case "Cover":
+                        c.DataPropertyName = "FrontCoverPath";
+                        c.Visible = false;
+                        break;
+
+                    case "TitleName":
+                        c.DataPropertyName = "Name";
+                        break;
+                    default:
+                        //c.Visible = false;
+                        break;
+                }
+            }
+        }
     }
 }
