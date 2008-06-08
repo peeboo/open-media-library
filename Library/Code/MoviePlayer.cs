18f3da8736a6e4d704e6e2f8f567012d89032a33
taranu <taranu@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2008-06-08 06:22:58 +0000

    Code cleanup

--- Library/Code/MoviePlayer.cs
+++ Library/Code/MoviePlayer.cs
@@ -79,31 +79,8 @@ namespace Library
             Utilities.DebugLine("MoviePlayerFactory.Transport_PropertyChanged: movie {0} property {1} playrate {2} state {3} pos {4}", OMLApplication.Current.NowPlayingMovieName, property, t.PlayRate, t.PlayState.ToString(), t.Position.ToString());
             if (property == "PlayState")
             {
-                if (t.PlayState == PlayState.Paused)
-                {
-                    OMLApplication.Current.NowPlayingStatus = "Paused";
-                    Utilities.DebugLine("MoviePlayerFactory.Transport_PropertyChanged: movie {0} Paused", OMLApplication.Current.NowPlayingMovieName);
-                }
-                else if (t.PlayState == PlayState.Playing)
-                {
-                    OMLApplication.Current.NowPlayingStatus = "Playing";
-                    Utilities.DebugLine("MoviePlayerFactory.Transport_PropertyChanged: movie {0} Playing", OMLApplication.Current.NowPlayingMovieName);
-                }
-                else if (t.PlayState == PlayState.Finished)
-                {
-                    Utilities.DebugLine("MoviePlayerFactory.Transport_PropertyChanged: movie {0} Finished", OMLApplication.Current.NowPlayingMovieName);
-                    OMLApplication.Current.NowPlayingStatus = "Finished";
-                }
-                else if (t.PlayState == PlayState.Stopped)
-                {
-                    OMLApplication.Current.NowPlayingStatus = "Stopped ";
-                    Utilities.DebugLine("MoviePlayerFactory.Transport_PropertyChanged: movie {0} Stopped", OMLApplication.Current.NowPlayingMovieName);
-                }
-                else if (t.PlayState == PlayState.Undefined)
-                {
-                    OMLApplication.Current.NowPlayingStatus = "Last Watched";
-                    Utilities.DebugLine("MoviePlayerFactory.Transport_PropertyChanged: movie {0} LastWatched", OMLApplication.Current.NowPlayingMovieName);
-                }
+                OMLApplication.Current.NowPlayingStatus = t.PlayState;
+                Utilities.DebugLine("MoviePlayerFactory.Transport_PropertyChanged: movie {0} state {1}", OMLApplication.Current.NowPlayingMovieName, t.PlayState.ToString());
             }
         }
 
