eebf5bc1de7c45e4a0fe879b3ce91427bbf885b3
taranu <taranu@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2008-06-10 12:09:53 +0000

    Added reference to OMLSDK and added TreeNodeEventArgs

--- Library/Code/TreeNodeEventArgs.cs
+++ Library/Code/TreeNodeEventArgs.cs
@@ -1,18 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Library
 {
     public class TreeNodeEventArgs : EventArgs
     {
-        private TreeNode _node = null;
-
         public TreeNodeEventArgs(TreeNode node)
         {
-            OMLApplication.DebugLine("Checked Node Changed: " + node.Title);
-            _node = node;
+            Node = node;
         }
 
         public TreeNode Node
@@ -20,5 +14,12 @@ namespace Library
             get { return _node; }
             set { _node = value; }
         }
+
+        #region Fields
+
+        private TreeNode _node = null;
+
+        #endregion
+
     }
 }
