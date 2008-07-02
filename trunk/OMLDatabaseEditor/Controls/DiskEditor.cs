a8facf4dec253dcd39ca4cc8850e9717051f4ec0
DJShultz <DJShultz@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2008-07-02 01:09:43 +0000

    sorry, I missed these

---
+++ trunk/OMLDatabaseEditor/Controls/DiskEditor.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OMLDatabaseEditor.Controls
+{
+    public partial class DiskEditor : Form
+    {
+        public DiskEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            //TODO: Validate the Path
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (chkFile.Checked)
+            {
+                OpenFileDialog dlg = new OpenFileDialog();
+                dlg.CheckPathExists = true;
+                dlg.FileName = txtPath.Text;
+                dlg.Multiselect = false;
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    txtPath.Text = dlg.FileName;
+                }
+            }
+            else
+            {
+                FolderBrowserDialog dlg = new FolderBrowserDialog();
+                dlg.SelectedPath = txtPath.Text;
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    txtPath.Text = dlg.SelectedPath;
+                }
+            }
+        }
+
+        public string FileName
+        {
+            get { return txtName.Text; }
+            set { txtName.Text = value; }
+        }
+
+        public string Path
+        {
+            get { return txtPath.Text; }
+            set { txtPath.Text = value; }
+        }
+
+    }
+}
