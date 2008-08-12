d7263c45fa244bc20f6edf73d92dd9cc378be2cb
DJShultz <DJShultz@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2008-08-12 05:28:50 +0000

    First draft at an AppleTrailers datasource

---
+++ trunk/AppleTrailers/TrailerData.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Xml;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace AppleTrailers
+{
+    public class TrailerData
+    {
+        public TrailerData()
+        {
+        }
+
+        public List<AppleTitle> GetAppleTitles()
+        {
+            List<AppleTitle> appleTitles = new List<AppleTitle>();
+
+            try
+            {
+                WebClient client = new WebClient();
+                Stream strm = client.OpenRead(@"http://www.apple.com/trailers/home/feeds/just_hd.json");
+                StreamReader sr = new StreamReader(strm);
+                string json = sr.ReadToEnd();
+                MemoryStream ms = new MemoryStream(System.Text.Encoding.Unicode.GetBytes(json));
+                DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(List<AppleTitle>));
+                appleTitles = ser.ReadObject(ms) as List<AppleTitle>;
+                ms.Close();
+            }
+            catch (Exception e) { }
+
+            return appleTitles;
+        }
+
+        [DataContract(Name="AppleTrailer", Namespace="")]
+        public class AppleTitle
+        {
+            string _location;
+
+            [DataMember]
+            public string title;
+            [DataMember]
+            public string releasedate;
+            [DataMember]
+            public string studio;
+            [DataMember]
+            public string poster;
+            [DataMember]
+            public string moviesite;
+            [DataMember]
+            public string location
+            {
+                get { return _location; }
+                set
+                {
+                    _location = @"www.apple.com" + value;
+                }
+            }
+            [DataMember]
+            public string director;
+            [DataMember]
+            public string rating;
+            [DataMember]
+            public List<string> genre;
+            [DataMember]
+            public List<string> actors;
+            [DataMember]
+            public List<Trailer> trailers;
+        }
+
+        [DataContract(Name="Trailer", Namespace="")]
+        public class Trailer
+        {
+            [DataMember]
+            public string postdate;
+            [DataMember]
+            public string url;
+            [DataMember]
+            public string type;
+            [DataMember]
+            public string exclusive;
+            [DataMember]
+            public bool hd;
+        }
+    }
+}
