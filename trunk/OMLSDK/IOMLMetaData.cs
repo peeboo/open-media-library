458e1dd2bf5d7de11b604c8e9596159192a98472
taranu <taranu@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2008-08-12 17:59:05 +0000

    Added new interface for meta data plugin (comments please!!!)

---
+++ trunk/OMLSDK/IOMLMetaData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OMLEngine;
+
+namespace OMLSDK
+{
+    public interface IOMLMetadataPlugin
+    {
+        string GetPluginName();
+
+        // these 2 methods must be called in sequence
+        bool Initialize( Dictionary<string,string> parameters );
+        bool SearchForMovie( string movieName );
+
+        // these methods are to be called after the 2 methods above
+
+        // get the best match
+        Title GetBestMatch();
+
+        // or choose among all the titles
+        Title[] GetAvailableTitles();    // could be just summaries
+        Title GetTitle(int index);      // get the actual Title
+
+        // can do additional searches
+        bool AdditonalSearchForMovie(string movieName);
+    }
+
+}
