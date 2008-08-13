1ff7b1733c343cade474844aa676caaac896135e
DJShultz <DJShultz@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2008-08-13 00:08:29 +0000

    Initial setup for players for hddvd and bluray

---
+++ trunk/Library/Code/MoviePlayerHDDVD.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using OMLEngine;
+using Microsoft.MediaCenter.Hosting;
+using Microsoft.MediaCenter;
+using Microsoft.MediaCenter.UI;
+using System.IO;
+
+namespace Library
+{
+    public class HDDVDPlayer : IPlayMovie
+    {
+        MovieItem _title;
+        public HDDVDPlayer(MovieItem title)
+        {
+            _title = title;
+        }
+
+        public bool PlayMovie()
+        {
+            string media = "HDDVD://" + _title.SelectedDisk.Path;
+            media.Replace('\\', '/');
+            if (AddInHost.Current.MediaCenterEnvironment.PlayMedia(MediaType.Video, media, false))
+            {
+                if (AddInHost.Current.MediaCenterEnvironment.MediaExperience != null)
+                {
+                    Utilities.DebugLine("HDDVDPlayer.PlayMovie: movie {0} Playing", _title.Name);
+                    OMLApplication.Current.NowPlayingMovieName = _title.Name;
+                    OMLApplication.Current.NowPlayingStatus = PlayState.Playing;
+                    AddInHost.Current.MediaCenterEnvironment.MediaExperience.Transport.PropertyChanged += MoviePlayerFactory.Transport_PropertyChanged;
+                    AddInHost.Current.MediaCenterEnvironment.MediaExperience.GoToFullScreen();
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
