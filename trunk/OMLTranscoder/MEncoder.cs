7c696051ebf2bd5768dbda87fe11ed23e82c99dd
DJShultz <DJShultz@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2008-08-19 21:58:11 +0000

    Started work on a data extractor, fleshed out more of the MEncoderCommandBuilder

---
+++ trunk/OMLTranscoder/MEncoder.cs
@@ -0,0 +1,18 @@
+﻿/*******************************************************
+ * This project heavily inspired by Transcode 360      *
+ * written by Albert Griscti-Soler and Bernard Maltais *
+ *******************************************************/
+
+using System;
+
+namespace OMLTranscoder
+{
+    public class MEncoder
+    {
+        public enum AudioFormat
+        { NoAudio, MP2, MP3, AC3 };
+
+        public enum VideoFormat
+        { Copy };
+    }
+}
--- trunk/OMLTranscoder/MEncoderCommandBuilder.cs
+++ trunk/OMLTranscoder/MEncoderCommandBuilder.cs
@@ -4,27 +4,68 @@
  *******************************************************/
 
 using System;
+using System.IO;
+using OMLEngine;
 
 namespace OMLTranscoder
 {
     public class MEncoderCommandBuilder
     {
-        string _mencoder_arguments_string = string.Empty;
+        private string outputFile;
+        private MEncoder.AudioFormat audioFormat;
+        private MEncoder.VideoFormat videoFormat;
 
         public MEncoderCommandBuilder()
         {
+            outputFile = string.Empty;
         }
 
-        public void SetVideoOutputFormat()
+        public void SetVideoOutputFormat(MEncoder.VideoFormat format)
         {
+            videoFormat = format;
         }
 
-        public void SetAudioOutputFormat()
+        public void SetAudioOutputFormat(MEncoder.AudioFormat format)
         {
+            audioFormat = format;
         }
 
         public void SetSubtitleOutputLanguage()
         {
+        }
+
+        public void SetOutputFile(string output)
+        {
+            outputFile = output;
+        }
+
+        public string GetCommand()
+        {
+            string cmdString = string.Empty;
+            cmdString = Path.Combine(FileSystemWalker.RootDirectory, @"mencoder.exe");
+
+            //audio
+            if (audioFormat == MEncoder.AudioFormat.NoAudio)
+                cmdString += @" -nosound";
+            else
+                cmdString += @" -oac " + (string)Enum.GetName(typeof(MEncoder.AudioFormat), audioFormat);
+
+            //video
+            cmdString += @" -ovc " + (string)Enum.GetName(typeof(MEncoder.VideoFormat), videoFormat);
+
+            //subtitles
+
+            //chapters
+
+            //output
+            cmdString += @" -o " + outputFile;
+
+            return cmdString;
+        }
+
+        public void PresetSubtitleDetailsComamnd()
+        {
+
         }
     }
 }
