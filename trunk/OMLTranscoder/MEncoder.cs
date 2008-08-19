dbf114d9139f93774263fd1c924f3e73303b8a72
DJShultz <DJShultz@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2008-08-19 22:57:55 +0000

    Restructure video/audio format options and build out MEncoderCommandBuilder class

--- trunk/OMLTranscoder/MEncoder.cs
+++ trunk/OMLTranscoder/MEncoder.cs
@@ -10,9 +10,24 @@ namespace OMLTranscoder
     public class MEncoder
     {
         public enum AudioFormat
-        { NoAudio, MP2, MP3, AC3 };
+        { 
+          NoAudio, // -nosound
+          PCM,     // -oac pcm
+          MP2,     // -oac twolame
+          MP3,     // -oac mp3lame
+          COPY,    // -oac copy
+          LAVC     // FFmpeg audio encoding (mp2, ac3)
+        };
 
         public enum VideoFormat
-        { Copy };
+        { 
+          Copy,      // -ovc copy
+          RAW,       // -ovc raw
+          LAVC,      // -ovc lavc libavcodec - best quality (this is usually divx)
+          VideoForWindows, // -ovc video for windows
+          Quicktime, // -ovc qtvideo - svq1/3 are supported
+          XviD,      // -ovc xvid
+          X264       // -ovc x264 H.264 encoding
+        };
     }
 }
