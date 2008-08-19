1bb6ef4857cdf5922eb7afc417097d1a9fe90049
taranu <taranu@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2008-08-19 21:16:16 +0000

    Added some comments and instructions

--- trunk/AmazonMetadata/AmazonMetaDataPlugin.cs
+++ trunk/AmazonMetadata/AmazonMetaDataPlugin.cs
@@ -1,25 +1,55 @@
-﻿using System;
+﻿
+// Sample Metadata plugin - uses AMazon web service
+
+using System;
 using System.Collections.Generic;
 using System.Text;
-using OMLEngine;
-using OMLSDK;
+using OMLEngine;        // need this for OML Title class
+using OMLSDK;           // need this for the IOMLMetadataPlugin
 
+// Step 1: Make sure that you add OMLEngine and OMLSDK to your References
+// Step 2: Make sure you add "using OMLENgine;" and "using OMSDK;" to your plugin code
+// Step 3: Create a class that impelments interface IOMLMetadataPlugin
+// Step 4: Implement the following methods from IOMLMetadatapluing:
+//  Step 5: Implement PluginName property. This name is used in Menus
+//  Step 6: Impelment Initialize method. You can do any initialization your plugin may need. If you don't need anything just return true;
+//  Step 7: Implement SearchForMovies. OML will pass you the movie name, your code goes and searches for possible matches.
+//          Use private data to keep whatever data you need around. Return true if the search was successful. You can keep an array or list of Title
+//          objects to be retrieved later by OML.
+//  Step 8: Impelment GetBestMatch. NOrmally returns the first Title on the list that was found using SearchForMovies.
+//  Step 9: Implement GetAvailableTitles. Return an array of Title objects that were found in SearchMovies. This is to give the user
+//          a chance to select the right movie. Note that you don't have to fill in all the fields in the Title object. For exapmle IMDB
+//          may return only the name and possibly the year when responding to the search, while Amazon usually retrieves everything.
+//  Step 10:  Implement GetTitle. OML asks for a Title at the index in the array from Step 9. At this point you may need to go back
+//            to the metadata provider and dig deeper to get the full details of the movie. In IMDB, another HTTP request
+//            has to be done (or possibly more, depending how much data you want). In Amazon it's easier, since the search gets the full
+//            details for all the movies it finds.
+//  Step 11: Implement GetOptions. Return null or an empty list if you plugin doesn't have any user settable options. The Amazon plugin
+//           has one option for the locale (website to use, UK, Canada, etc). If you have options, you need to pass all the possible values to it.
+//  Step 12: Implement SetOptionValue. Return true or false if you don't have options.
+//  The options can be set in DBEditor.
+// Step 13: compile and make sure you copy the plugin to the OML plugins directory.
 namespace AmazonMetadata
 {
     public class AmazonMetadataPlugin : IOMLMetadataPlugin
     {
-        // don't initialize it yet since there's a small time penalty
-        // to initialize the web service.
+        // Amazon specific data
+        // use whatever data you need for your search engine
         AmazonWebService _amazon = null;
         AmazonLocale _locale = AmazonLocale.Default;
         AmazonSearchResult _searchResult = null;
 
+        //
+        // must implement this, will show up in the menus
+        //
         public string PluginName
         {
             get { return "Amazon"; }
         }
 
-        // these 2 methods must be called in sequence
+        // Must call this before any other plugin method
+        // parameters is optional -use it if you want to get parameters passed in
+        // here we can accept the Locale parameter but we'll use the default if not
         public bool Initialize(Dictionary<string, string> parameters)
         {
             try
@@ -45,6 +75,9 @@ namespace AmazonMetadata
             }
         }
 
+        // this is the main method used to actually search for movies
+        // returns true if it succeeded
+        // Note: save the results in your private variables, we'll ask later for the found items
         public bool SearchForMovie(string movieName)
         {
             try
@@ -68,15 +101,8 @@ namespace AmazonMetadata
             }
         }
 
-        public bool AdditonalSearchForMovie(string movieName)
-        {
-            // dont' do any more (we could do all pages)
-            return false;
-        }
-
-        // these methods are to be called after the 2 methods above
-
-        // get the best match
+       
+        // get the best match after doing a SearchFOrMovies
         public Title GetBestMatch()
         {
             if (_searchResult != null & _searchResult.DVDList != null)
@@ -90,7 +116,7 @@ namespace AmazonMetadata
             }
         }
 
-        // or choose among all the titles
+        // or choose among all the titles after doing a SearchFOrMovies
         public Title[] GetAvailableTitles()
         {
             if (_searchResult != null)
@@ -99,6 +125,7 @@ namespace AmazonMetadata
                 return null;
         }
 
+        // gets a Title based on the index in GetAvailableTitles() above
         public Title GetTitle(int index)
         {
             if (_searchResult != null & _searchResult.DVDList != null)
@@ -119,6 +146,8 @@ namespace AmazonMetadata
             }
         }
 
+        // if your plugin has any options than can be set send them here
+        // otherwise return null
         public List<OMLMetadataOption> GetOptions()
         {
             List<OMLMetadataOption> settings = new List<OMLMetadataOption>();
@@ -134,6 +163,7 @@ namespace AmazonMetadata
             return settings;
         }
 
+        // sets an option. if you don't handle this just ignore it
         public bool SetOptionValue(string option, string value)
         {
             if (option.Equals("Locale", StringComparison.CurrentCultureIgnoreCase))
