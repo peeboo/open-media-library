ff744455f4ef59ffe4a737a0b8582a11d4dced65
DJShultz <DJShultz@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2008-08-19 20:29:15 +0000

    More framework

---
+++ OMLTranscoder/SubtitleStream.cs
@@ -0,0 +1,23 @@
+﻿/*******************************************************
+ * This project heavily inspired by Transcode 360      *
+ * written by Albert Griscti-Soler and Bernard Maltais *
+ *******************************************************/
+
+using System;
+
+namespace OMLTranscoder
+{
+    public class SubtitleStream
+    {
+        string languageId;
+        int subtitleId;
+        string subtitleChannel;
+
+        public SubtitleStream()
+        {
+            languageId = string.Empty;
+            subtitleId = -1;
+            subtitleChannel = string.Empty;
+        }
+    }
+}
