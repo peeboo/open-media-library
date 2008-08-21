ed9ca2cacc41227ca984a575878b3d67a73fef89
DJShultz <DJShultz@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2008-08-21 00:22:18 +0000

    Added code to delete the old registry key for extenders if it exists to the launch method

--- Library/Code/Launch.cs
+++ Library/Code/Launch.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using Microsoft.MediaCenter.Hosting;
+using Microsoft.Win32;
 
 namespace Library
 {
@@ -35,6 +36,7 @@ namespace Library
         public void Launch(AddInHost host)
         {
             OMLApplication.DebugLine("[Launch] Launch called");
+            DeleteOldRegistryKey(host);
             s_session = new HistoryOrientedPageSession();
 
             if (app == null)
@@ -44,6 +46,38 @@ namespace Library
             }
 
             app.Startup(_id);
+        }
+
+        private void DeleteOldRegistryKey(AddInHost host)
+        {
+            // remove old HKCU key that caused settings to appear on the TV+Movies menu strip
+            if (!host.MediaCenterEnvironment.Capabilities.ContainsKey("Console"))
+            {
+                try
+                {
+                    RegistryKey delKeyValue = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\" +
+                                                                              @"Media Center\Extensibility\Categories\Services\" +
+                                                                              @"Movies\{543d0438-b10d-43d8-a20d-f0c96db4e6bd}", true);
+                    if (delKeyValue != null)
+                    {
+                        OMLApplication.DebugLine("[Launch] Deleting old registry key value");
+                        delKeyValue.DeleteValue(@"UseCount");
+                    }
+                }
+                catch (Exception) { } // we don't really care, just delete it if you find it
+
+                try
+                {
+                    RegistryKey delSubKey = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\" +
+                                                                            @"Media Center\Extensibility\Categories\Services\Movies", true);
+                    if (delSubKey != null)
+                    {
+                        OMLApplication.DebugLine("[Launch] Deleting old registry key");
+                        delSubKey.DeleteSubKey(@"{543d0438-b10d-43d8-a20d-f0c96db4e6bd}");
+                    }
+                }
+                catch (Exception) { } // we don't really care, just delete it if you find it
+            }
         }
     }
 }
