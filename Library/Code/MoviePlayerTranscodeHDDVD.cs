6aa921e7025a566f145d6405f014476e4ce03809
DJShultz <DJShultz@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2008-08-22 19:26:49 +0000

    Setting up bluray/hddvd playback primitives and transcoding libs

---
+++ Library/Code/MoviePlayerTranscodeHDDVD.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using OMLEngine;
+
+namespace Library
+{
+    public class TranscodeHDDVDPlayer: IPlayMovie
+    {
+        /*******************************************************************************
+         * Either AnyHDDVD is required OR the disc must have already been ripped to HD *
+         * A VERY powerful machine is required for realtime transcoding                *
+         *******************************************************************************/
+
+// command to transcode hddvd is:
+// mencoder <filename.evo> -o <output filename> -oac copy -ovc lavc -lavcopts codec=???:vbitrate=???? -fps <30|60> -vf -scale=1280:720
+// The audio streams will have to be reviewed for NON HD/TRU audio channels, we need either DTSHD, DTS, DD, or something else useful.
+// NOTE: DTSHD is completely compatible with DTS, while Dolby Digital HD is NOT compatible with Dolby Digital
+
+        private MovieItem _title;
+
+        public TranscodeHDDVDPlayer(MovieItem title)
+        {
+            _title = title;
+        }
+
+        public bool PlayMovie()
+        {
+        }
+
+    }
+}
