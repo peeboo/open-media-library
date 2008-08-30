4c83fe22eb2749bd478c85c545be27b47d5f1196
DJShultz <DJShultz@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2008-08-30 19:58:34 +0000

    forgot a file

---
+++ OMLTestSuite/MoviePlayerDVDTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Library;
+using OMLEngine;
+using NUnit.Framework;
+
+namespace OMLTestSuite
+{
+    [TestFixture]
+    public class MoviePlayerDVDTest
+    {
+        [Test]
+        public void TEST_GENERATE_STRING_FOR_A_STANDARD_DVD()
+        {
+            string originalPath = "..\\..\\..\\Sample Files\\TestFolder\\TestFolder1";
+
+            Assert.AreEqual("DVD://../../../Sample Files/TestFolder/TestFolder1/VIDEO_TS",
+                            DVDPlayer.GeneratePlayString(originalPath, 0, 0));
+        }
+
+        [Test]
+        public void TEST_GENERATE_STRING_CORRECTLY_BUILDS_TITLE_SELECTION_STRING()
+        {
+            string originalPath = "..\\..\\..\\Sample Files\\TestFolder\\TestFolder1";
+
+            Assert.AreEqual("DVD://../../../Sample Files/TestFolder/TestFolder1/VIDEO_TS?2",
+                            DVDPlayer.GeneratePlayString(originalPath, 2, 0));
+        }
+
+        [Test]
+        public void TEST_GENERATE_STRING_CORRECTLY_BUILDS_TITLE_AND_CHAPTER_SELECTION_STRING()
+        {
+            string originalPath = "..\\..\\..\\Sample Files\\TestFolder\\TestFolder1";
+
+            Assert.AreEqual("DVD://../../../Sample Files/TestFolder/TestFolder1/VIDEO_TS?2/5",
+                            DVDPlayer.GeneratePlayString(originalPath, 2, 5));
+        }
+
+        [Test]
+        public void TEST_GENERATE_STRING_CORRECTLY_BUILDS_START_TIME()
+        {
+            string originalPath = "..\\..\\..\\Sample Files\\TestFolder\\TestFolder1";
+
+            Assert.AreEqual("DVD://../../../Sample Files/TestFolder/TestFolder1/VIDEO_TS?1/0:18:32",
+                            DVDPlayer.GeneratePlayString(originalPath, 1, new DateTime(2008, 01, 01, 0, 18, 32)));
+        }
+    }
+}
