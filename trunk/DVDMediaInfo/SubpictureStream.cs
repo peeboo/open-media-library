70b19c94e3b982e4abbf1727450a46a683619300
DJShultz <DJShultz@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2008-09-03 04:40:17 +0000

    Fleshed out the rest of the initial set of methods on the DVDInfo class, now its almost ready for use in the transcoder

---
+++ trunk/DVDMediaInfo/SubpictureStream.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DVDMediaInfo
+{
+    public class SubpictureStream
+    {
+        private string subpictureCoding;
+        private int languageId;
+        private string languageExtension;
+        private string subpictureType;
+
+        public string Coding
+        {
+            get { return subpictureCoding; }
+            set { subpictureCoding = value; }
+        }
+
+        public int LanguageId
+        {
+            get { return languageId; }
+            set { languageId = value; }
+        }
+
+        public string LanguageExtension
+        {
+            get { return languageExtension; }
+            set { languageExtension = value; }
+        }
+
+        public string Type
+        {
+            get { return subpictureType; }
+            set { subpictureType = value; }
+        }
+
+        public SubpictureStream()
+        {
+        }
+    }
+}
