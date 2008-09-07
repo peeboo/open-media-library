08ba17876d1f73216d714155547428e1bbc12c9c
DJShultz <DJShultz@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2008-09-07 18:13:14 +0000

    Redesigning layout of files for new visual layout, creating a base class for all UI pages (both mcml and c#)

---
+++ Library/Code/LayoutV2/BasePage.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.MediaCenter;
+using Microsoft.MediaCenter.Hosting;
+using Microsoft.MediaCenter.UI;
+
+namespace Library
+{
+    public class BasePage : ModelItem
+    {
+        #region private variables
+        private bool showWaitCursor;
+        private bool startingTranscodeJob;
+        private Image customBackgroundImage;
+        #endregion
+
+        #region properties
+        public bool ShowWaitCursor
+        {
+            get
+            {
+                return showWaitCursor;
+            }
+            set
+            {
+                showWaitCursor = value;
+                FirePropertyChanged("ShowWaitCursor");
+            }
+        }
+
+        public bool StartingTranscodeJob
+        {
+            get
+            {
+                return startingTranscodeJob;
+            }
+            set
+            {
+                startingTranscodeJob = value;
+                FirePropertyChanged("StartingTranscodeJob");
+            }
+        }
+
+        public Image CustomBackgroundImage
+        {
+            get
+            {
+                return customBackgroundImage;
+            }
+            set
+            {
+                customBackgroundImage = value;
+                FirePropertyChanged("CustomBackgroundImage");
+            }
+        }
+
+        public OMLApplication App
+        {
+            get
+            {
+                return OMLApplication.Current;
+            }
+        }
+
+        public AddInHost Host
+        {
+            get
+            {
+                return AddInHost.Current;
+            }
+        }
+
+        public HistoryOrientedPageSession Session
+        {
+            get
+            {
+                return App.Session;
+            }
+        }
+
+
+        #endregion
+
+        public BasePage()
+        {
+        }
+    }
+}
