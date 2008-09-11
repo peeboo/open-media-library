b8a94e95b25cc75b9e02fc5482200427d91aed4b
DJShultz <DJShultz@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2008-09-11 23:59:46 +0000

    Added non fatal error reporting from importers

---
+++ trunk/OMLDatabaseEditor/Controls/NonFatalErrors.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace OMLDatabaseEditor.Controls
+{
+    partial class NonFatalErrors
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbNonFatalErrors = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbNonFatalErrors
+            // 
+            this.tbNonFatalErrors.Location = new System.Drawing.Point(12, 12);
+            this.tbNonFatalErrors.Multiline = true;
+            this.tbNonFatalErrors.Name = "tbNonFatalErrors";
+            this.tbNonFatalErrors.ReadOnly = true;
+            this.tbNonFatalErrors.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbNonFatalErrors.Size = new System.Drawing.Size(252, 110);
+            this.tbNonFatalErrors.TabIndex = 0;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(101, 128);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // NonFatalErrors
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(276, 154);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.tbNonFatalErrors);
+            this.Name = "NonFatalErrors";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "The Following NON fatal errors occured";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbNonFatalErrors;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
