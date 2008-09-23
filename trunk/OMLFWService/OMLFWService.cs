b7eab4d35bd3255ba57396e1f0c5b632d813a0a0
DJShultz <DJShultz@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2008-09-23 20:18:54 +0000

    Service changes and updates preparing them for use

--- trunk/OMLFWService/OMLFWService.cs
+++ trunk/OMLFWService/OMLFWService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.ServiceProcess;
+using System.Timers;
 using System.Text;
 using Microsoft.Win32;
 
@@ -17,6 +18,7 @@ namespace OMLFWService
         String[] watch;
         Boolean logging = false;
         Boolean subdirs;
+        Timer _timer;
 
         public OMLFWService()
         {
@@ -29,6 +31,14 @@ namespace OMLFWService
             rkOML = rkSoftware.OpenSubKey(@"OML");
             rkSoftware.Close();
             rkCU.Close();
+
+            _timer = new Timer(20000);
+            _timer.Elapsed +=new ElapsedEventHandler(_timer_Elapsed);
+
+            this.CanStop = true;
+            this.CanPauseAndContinue = true;
+            this.AutoLog = true;
+
             if (!Boolean.TryParse((string)rkOML.GetValue("logging", logging), out logging))
             {
                 logging = false;
@@ -56,18 +66,58 @@ namespace OMLFWService
             }
         }
 
+        protected override void OnPause()
+        {
+            base.OnPause();
+            fw.EnableRaisingEvents = false;
+            _timer.Stop();
+            WriteToLog(EventLogEntryType.Information, "OMLFWService Paused");
+        }
+
+        protected override void OnContinue()
+        {
+            base.OnContinue();
+            fw.EnableRaisingEvents = true;
+            _timer.Start();
+            WriteToLog(EventLogEntryType.Information, "OMLFWService Continued");
+        }
+
         protected override void OnStart(string[] args)
         {
             // TODO: Add code here to start your service.
             fw.EnableRaisingEvents = true;
+            _timer.Start();
+            WriteToLog(EventLogEntryType.Information, "OMLFWService Started");
         }
 
         protected override void OnStop()
         {
             // TODO: Add code here to perform any tear-down necessary to stop your service.
             fw.EnableRaisingEvents = false;
+            _timer.Stop();
+            WriteToLog(EventLogEntryType.Information, "OMLFWService Stopped");
         }
 
+        private void WriteToLog(EventLogEntryType type, string msg)
+        {
+            string sSource = @"OMLFWService";
+            string sLog = string.Empty;
+
+            if (!EventLog.SourceExists(sSource))
+                EventLog.CreateEventSource(sSource, sLog);
+
+            EventLog evt = new EventLog(sLog);
+            string message = msg + ": " +
+                                   DateTime.Now.ToShortDateString() + " " +
+                                   DateTime.Now.ToLongTimeString();
+            evt.Source = sSource;
+            evt.WriteEntry(message, type);
+        }
+
+        protected void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            WriteToLog(EventLogEntryType.Information, "OMLFWService Timer triggered");
+        }
     }
 
 }
