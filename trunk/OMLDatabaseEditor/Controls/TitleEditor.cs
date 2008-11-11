6d28165b5bb44ca31e805b51d58976b0aed4e3ce
RobEpstein74 <RobEpstein74@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2008-11-11 03:59:16 +0000

    All movie editing and adding functionality present. Only missing functionality is ability to edit movie details involving people (Director, Actors, etc). This represents the first revision of the editor that uses the new UI.

---
+++ trunk/OMLDatabaseEditor/Controls/TitleEditor.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Windows.Forms;
+
+using OMLEngine;
+
+namespace OMLDatabaseEditor.Controls
+{
+    public partial class TitleEditor : UserControl
+    {
+        public delegate void TitleChangeEventHandler(object sender, EventArgs e);
+        public delegate void TitleNameChangeEventHandler(object sender, TitleNameChangedEventArgs e);
+        public delegate void SavedEventHandler(object sender, EventArgs e);
+
+        public event TitleChangeEventHandler TitleChanged;
+        public event TitleNameChangeEventHandler TitleNameChanged;
+        public event SavedEventHandler SavedTitle;
+
+        private bool _isLoading = false;
+
+        public enum TitleStatus { Normal, UnsavedChanges }
+
+        private TitleStatus _status;
+        public TitleStatus Status
+        {
+            get { return _status; }
+            set { _status = value; }
+        }
+
+        private Title _dvdTitle;
+        public Title EditedTitle
+        {
+            get { return _dvdTitle; }
+        }
+
+        public TitleEditor()
+        {
+            InitializeComponent();
+        }
+
+        public void LoadDVD(Title dvd)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (MemoryStream stream = new MemoryStream())
+            {
+                formatter.Serialize(stream, dvd);
+                stream.Position = 0;
+                _dvdTitle = (Title)formatter.Deserialize(stream);
+            }
+            _isLoading = true;
+            titleSource.DataSource = _dvdTitle;
+            Status = TitleStatus.Normal;
+            _isLoading = false;
+        }
+
+        public void RefreshEditor()
+        {
+            titleSource.ResetCurrentItem();
+        }
+
+        public void ClearEditor()
+        {
+            _dvdTitle = null;
+            titleSource.DataSource = typeof(Title);
+            Status = TitleStatus.Normal;
+        }
+
+        private void _titleChanged(EventArgs e)
+        {
+            if (TitleChanged != null && Status != TitleStatus.UnsavedChanges && !_isLoading)
+            {
+                _status = TitleStatus.UnsavedChanges;
+                TitleChanged(this, e);
+            }
+        }
+
+        private void _titleNameChanged(TitleNameChangedEventArgs e)
+        {
+            if (TitleNameChanged != null && !_isLoading)
+            {
+                _status = TitleStatus.UnsavedChanges;
+                TitleNameChanged(this, e);
+            }
+        }
+
+        private void TitleChanges(object sender, EventArgs e)
+        {
+            _titleChanged(EventArgs.Empty);
+        }
+
+        private void TitleNameChanges(object sender, EventArgs e)
+        {
+            _titleNameChanged(new TitleNameChangedEventArgs(txtName.Text));
+        }
+
+        private void EditList(string name, List<string> listToEdit)
+        {
+            ListEditor editor = new ListEditor(name, listToEdit);
+            List<string> original = listToEdit.ToList<string>();
+            editor.ShowDialog();
+            if (listToEdit.Union(original).Count<string>() != original.Count)
+            {
+                TitleChanges(null, null);
+            }
+        }
+
+        private void btnDisks_Click(object sender, EventArgs e)
+        {
+            DiskEditorFrm diskEditor = new DiskEditorFrm(EditedTitle.Disks);
+            diskEditor.ShowDialog();
+        }
+
+        private void pbCovers_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (_dvdTitle == null) return;
+            if (e.Button == MouseButtons.Right)
+            {
+                contextImage.Tag = sender;
+                contextImage.Show(sender as PictureBox, e.Location);
+            }
+        }
+
+        private void selectImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openCoverFile.ShowDialog() == DialogResult.OK)
+            {
+                PictureBox pb = contextImage.Tag as PictureBox;
+                if (pb.Name.Contains("Front"))
+                    _dvdTitle.CopyFrontCoverFromFile(openCoverFile.FileName, false);
+                else
+                    _dvdTitle.CopyBackCoverFromFile(openCoverFile.FileName, false);
+                titleSource.ResetCurrentItem();
+            }
+        }
+
+        private void btnGenres_Click(object sender, EventArgs e)
+        {
+            EditList("Genres", _dvdTitle.Genres);
+        }
+
+        private void btnTags_Click(object sender, EventArgs e)
+        {
+            EditList("Tags", _dvdTitle.Tags);
+        }
+
+        private void btnExtras_Click(object sender, EventArgs e)
+        {
+            EditList("Extra Features", _dvdTitle.ExtraFeatures);
+        }
+
+        private void btnTracks_Click(object sender, EventArgs e)
+        {
+            EditList("Audio Tracks", _dvdTitle.AudioTracks);
+        }
+
+        private void btnSubtitles_Click(object sender, EventArgs e)
+        {
+            EditList("Subtitles", _dvdTitle.Subtitles);
+        }
+
+        private void btnTrailers_Click(object sender, EventArgs e)
+        {
+            EditList("Trailers", _dvdTitle.Trailers);
+        }
+    }
+
+    public class TitleNameChangedEventArgs : EventArgs
+    {
+        private string _newName = string.Empty;
+        public string NewName
+        {
+            get { return _newName; }
+        }
+
+        public TitleNameChangedEventArgs(string newName)
+        {
+            _newName = newName;
+        }
+    }
+}
---
+++ trunk/OMLDatabaseEditor/ListEditor.Designer.cs
@@ -0,0 +1,102 @@
+namespace OMLDatabaseEditor
+{
+    partial class ListEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ListEditor));
+            this.lbItems = new DevExpress.XtraEditors.ListBoxControl();
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.btnItem = new DevExpress.XtraEditors.ButtonEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.lbItems)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnItem.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbItems
+            // 
+            this.lbItems.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbItems.HotTrackItems = true;
+            this.lbItems.Location = new System.Drawing.Point(0, 0);
+            this.lbItems.Name = "lbItems";
+            this.lbItems.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lbItems.Size = new System.Drawing.Size(216, 244);
+            this.lbItems.SortOrder = System.Windows.Forms.SortOrder.Ascending;
+            this.lbItems.TabIndex = 0;
+            this.lbItems.KeyDown += new System.Windows.Forms.KeyEventHandler(this.lbItems_KeyDown);
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Controls.Add(this.btnItem);
+            this.panelControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelControl1.Location = new System.Drawing.Point(0, 215);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(216, 29);
+            this.panelControl1.TabIndex = 3;
+            // 
+            // btnItem
+            // 
+            this.btnItem.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnItem.Location = new System.Drawing.Point(2, 2);
+            this.btnItem.Name = "btnItem";
+            this.btnItem.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Plus)});
+            this.btnItem.Size = new System.Drawing.Size(212, 20);
+            this.btnItem.TabIndex = 0;
+            this.btnItem.ButtonClick += new DevExpress.XtraEditors.Controls.ButtonPressedEventHandler(this.btnItem_ButtonClick);
+            // 
+            // ListEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(216, 244);
+            this.Controls.Add(this.panelControl1);
+            this.Controls.Add(this.lbItems);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ListEditor";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "ListEditor";
+            ((System.ComponentModel.ISupportInitialize)(this.lbItems)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.btnItem.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.ListBoxControl lbItems;
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.XtraEditors.ButtonEdit btnItem;
+    }
+}
