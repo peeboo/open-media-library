248141476cc973ca28995bada1e737c3205d01db
DJShultz <DJShultz@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2008-11-14 01:29:19 +0000

    Lots of small changes, this is all to get the WCF services up and running.  They are very, very close now.  Once they are up and running we can begin testing/debugging transcoding and auto FS monitoring

--- trunk/OMLEngineService/OMLEngineService.cs
+++ trunk/OMLEngineService/OMLEngineService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.ServiceModel;
 using System.ServiceProcess;
@@ -10,78 +11,47 @@ namespace OMLEngineService
 {
     public partial class OMLEngineService : ServiceBase
     {
-        private TitleCollection tc;
-        private Timer _timer;
+        private List<Title> titles;
         private ServiceHost _transcodingServiceHost, _titleCollectionServiceHost;
-
-        public enum Commands
-        {
-            WriteToLog = 128
-        }
-
-        protected override void OnCustomCommand(int command)
-        {
-            base.OnCustomCommand(command);
-
-            switch (command)
-            {
-                case (int)Commands.WriteToLog:
-                    break;
-                default:
-                    WriteToLog(EventLogEntryType.Error, "I have NO idea what command you wanted " + command);
-                    break;
-            }
-        }
 
         public OMLEngineService()
         {
             this.ServiceName = @"OMLEngineService";
-            _timer = new Timer(60000); // 1 minute
-            _timer.Elapsed += new ElapsedEventHandler(_timer_Elapsed);
 
             this.CanStop = true;
             this.CanPauseAndContinue = false;
             this.AutoLog = true;
 
-            tc = new TitleCollection();
+            titles = GetCurrentTitles();
+        }
+
+        private List<Title> GetCurrentTitles()
+        {
+            TitleCollection tc = new TitleCollection();
             tc.loadTitleCollection();
+            List<Title> titles = new List<Title>();
+            foreach (Title t in tc)
+            {
+                titles.Add(t);
+            }
+            return titles;
         }
 
         #region overridden control methods (start, stop, pause, continue, etc)
         protected override void OnStart(string[] args)
         {
-//            System.Diagnostics.Debugger.Launch();
+            System.Diagnostics.Debugger.Launch();
             WriteToLog(EventLogEntryType.Information, "OMLEngineService Start");
-            _timer.Start();
 
             _transcodingServiceHost = WCFUtilites.StartService(EventSource, typeof(TranscodingService));
             _titleCollectionServiceHost = WCFUtilites.StartService(EventSource, typeof(TitleCollectionAPI));
         }
 
-        protected override void OnContinue()
-        {
-            base.OnContinue();
-            _timer.Start();
-        }
-
-        protected override void OnPause()
-        {
-            base.OnPause();
-            _timer.Stop();
-        }
-
         protected override void OnStop()
         {
-            _timer.Stop();
             WCFUtilites.StopService(EventSource, ref _transcodingServiceHost);
             WCFUtilites.StopService(EventSource, ref _titleCollectionServiceHost);
             WriteToLog(EventLogEntryType.Information, "OMLEngineService Stop");
-        }
-
-        protected override void OnShutdown()
-        {
-            base.OnShutdown();
-            _timer.Stop();
         }
         #endregion
 
@@ -89,11 +59,6 @@ namespace OMLEngineService
         internal static void WriteToLog(EventLogEntryType type, string msg, params object[] args)
         {
             WCFUtilites.WriteToLog(EventSource, type, msg, args);
-        }
-
-        protected void _timer_Elapsed(object sender, ElapsedEventArgs e)
-        {
-            // WriteToLog(EventLogEntryType.Information, "OMLEngineService Timer triggered");
         }
     }
 }
