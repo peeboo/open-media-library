b7f1196fbfb3f7db7310cb86e06c9d308601f9fd
umuff71 <umuff71@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2008-11-17 04:05:38 +0000

    OML Engine Service: added logging, added comment to indicate what to do to fix endpoint startup failure

--- trunk/OMLEngineService/OMLEngineService.cs
+++ trunk/OMLEngineService/OMLEngineService.cs
@@ -40,7 +40,9 @@ namespace OMLEngineService
         #region overridden control methods (start, stop, pause, continue, etc)
         protected override void OnStart(string[] args)
         {
+#if DEBUG
             System.Diagnostics.Debugger.Launch();
+#endif
             WriteToLog(EventLogEntryType.Information, "OMLEngineService Start");
 
             _transcodingServiceHost = WCFUtilites.StartService(EventSource, typeof(TranscodingService));
