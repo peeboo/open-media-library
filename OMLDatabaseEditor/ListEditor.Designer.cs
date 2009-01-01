b0cc46b5c8393e366b086b49737e9bccf19760ff
thomlamb <thomlamb@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2009-01-01 00:12:13 +0000

    Minor control resizing and rearranging

--- OMLDatabaseEditor/ListEditor.Designer.cs
+++ OMLDatabaseEditor/ListEditor.Designer.cs
@@ -30,11 +30,8 @@ namespace OMLDatabaseEditor
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ListEditor));
             this.lbItems = new DevExpress.XtraEditors.ListBoxControl();
-            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
             this.cbeItem = new DevExpress.XtraEditors.ComboBoxEdit();
             ((System.ComponentModel.ISupportInitialize)(this.lbItems)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
-            this.panelControl1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.cbeItem.Properties)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,29 +42,21 @@ namespace OMLDatabaseEditor
             this.lbItems.Location = new System.Drawing.Point(0, 0);
             this.lbItems.Name = "lbItems";
             this.lbItems.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
-            this.lbItems.Size = new System.Drawing.Size(216, 244);
+            this.lbItems.Size = new System.Drawing.Size(216, 224);
             this.lbItems.SortOrder = System.Windows.Forms.SortOrder.Ascending;
             this.lbItems.TabIndex = 0;
             this.lbItems.KeyDown += new System.Windows.Forms.KeyEventHandler(this.lbItems_KeyDown);
             // 
-            // panelControl1
-            // 
-            this.panelControl1.Controls.Add(this.cbeItem);
-            this.panelControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panelControl1.Location = new System.Drawing.Point(0, 215);
-            this.panelControl1.Name = "panelControl1";
-            this.panelControl1.Size = new System.Drawing.Size(216, 29);
-            this.panelControl1.TabIndex = 3;
-            // 
             // cbeItem
             // 
-            this.cbeItem.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.cbeItem.Location = new System.Drawing.Point(2, 2);
+            this.cbeItem.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.cbeItem.Location = new System.Drawing.Point(0, 224);
             this.cbeItem.Name = "cbeItem";
+            this.cbeItem.Padding = new System.Windows.Forms.Padding(2);
             this.cbeItem.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo),
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Plus)});
-            this.cbeItem.Size = new System.Drawing.Size(212, 20);
+            this.cbeItem.Size = new System.Drawing.Size(216, 20);
             this.cbeItem.TabIndex = 1;
             this.cbeItem.ButtonClick += new DevExpress.XtraEditors.Controls.ButtonPressedEventHandler(this.cbeItem_ButtonClick);
             // 
@@ -76,8 +65,8 @@ namespace OMLDatabaseEditor
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(216, 244);
-            this.Controls.Add(this.panelControl1);
             this.Controls.Add(this.lbItems);
+            this.Controls.Add(this.cbeItem);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -87,8 +76,6 @@ namespace OMLDatabaseEditor
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "ListEditor";
             ((System.ComponentModel.ISupportInitialize)(this.lbItems)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
-            this.panelControl1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.cbeItem.Properties)).EndInit();
             this.ResumeLayout(false);
 
@@ -97,7 +84,6 @@ namespace OMLDatabaseEditor
         #endregion
 
         private DevExpress.XtraEditors.ListBoxControl lbItems;
-        private DevExpress.XtraEditors.PanelControl panelControl1;
         private DevExpress.XtraEditors.ComboBoxEdit cbeItem;
     }
 }
