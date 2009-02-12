a9dfc0ebc443fcbfe3b8c966a7da04bc77332f3c
mikem2te <mikem2te@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2009-02-12 07:37:34 +0000

    Small update to hopefully stop exception when adding a new title

--- OMLDatabaseEditor/Controls/TitleEditor.cs
+++ OMLDatabaseEditor/Controls/TitleEditor.cs
@@ -129,18 +129,21 @@ namespace OMLDatabaseEditor.Controls
         private void LoadBackdrops()
         {
             tblBackdrops.Controls.Clear();
-            if (Directory.Exists(_dvdTitle.BackDropFolder))
+            if (_dvdTitle.BasePath() != null)
             {
-                string[] images = Directory.GetFiles(_dvdTitle.BackDropFolder);
-                foreach (string image in images)
+                if (Directory.Exists(_dvdTitle.BackDropFolder))
                 {
-                    PictureBox pb = new PictureBox();
-                    pb.ImageLocation = image;
-                    pb.Height = 150;
-                    pb.Dock = DockStyle.Fill;
-                    pb.SizeMode = PictureBoxSizeMode.Zoom;
-                    //if (pb.Image == null) continue;
-                    tblBackdrops.Controls.Add(pb);
+                    string[] images = Directory.GetFiles(_dvdTitle.BackDropFolder);
+                    foreach (string image in images)
+                    {
+                        PictureBox pb = new PictureBox();
+                        pb.ImageLocation = image;
+                        pb.Height = 150;
+                        pb.Dock = DockStyle.Fill;
+                        pb.SizeMode = PictureBoxSizeMode.Zoom;
+                        //if (pb.Image == null) continue;
+                        tblBackdrops.Controls.Add(pb);
+                    }
                 }
             }
         }
