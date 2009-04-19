423cbd53ca20952e86309ed437d05c803244bb64
intemperance <intemperance@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2009-04-19 00:16:03 +0000

    adding the ability to save custom user filters that are made up of a list of filters
    this is just a first blush, it's going to need some work to work correctly with filters like "added today" or "unwatched"
    
    also moving the apple trailer API code into the OMLEngine
    
    fixing a bug with genres not returning

--- OMLEngine/Dao/TitleCollectionDao.cs
+++ OMLEngine/Dao/TitleCollectionDao.cs
@@ -480,7 +480,7 @@ namespace OMLEngine.Dao
             var filteredTitles = from t in GetFilteredTitlesWrapper(filters)
                                                 from g in t.Genres
                                                 join b in DBContext.Instance.GenreMetaDatas on g.GenreMetaDataId equals b.Id
-                                                join c in DBContext.Instance.ImageMappings on t.Id equals c.ImageId 
+                                                join c in DBContext.Instance.ImageMappings on t.Id equals c.TitleId                                                 
                                                     where c.ImageType == ImageType.FrontCoverImage
                                                 select new { GenreName = b.Name, Path = c.ImageId };
 
