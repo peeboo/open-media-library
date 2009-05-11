946606188d20442eec2977b9e30448eacb0964e5
mikem2te <mikem2te@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2009-05-11 09:22:27 +0000

    Adding new wix setup files.
    1. (Hopefully) Fixed issue where X64 installer does not correctly setup the ui library in VMC.
    
    2. Setup now creates a client and a server installer (the server installer embeds the ms-sql installer in the installer ready for installation).
    
    4. When installing, options are presented eg for the server installer, options are client components only, server components only or both .
    
    3. Once installer runs, it kicks off the postinstallerwizard to do the sql installation stuff (install and create schema etc).

---
+++ Library/Setup/Build.bat
@@ -0,0 +1,53 @@
+@ECHO OFF
+
+SET BUILD_TYPE=Release
+if "%1" == "Debug" set BUILD_TYPE=Debug
+
+REM Determine whether we are on an 32 or 64 bit machine
+if "%PROCESSOR_ARCHITECTURE%"=="x86" if "%PROCESSOR_ARCHITEW6432%"=="" goto x86
+
+set ProgramFilesPath=%ProgramFiles(x86)%
+
+goto startInstall
+
+:x86
+
+set ProgramFilesPath=%ProgramFiles%
+
+:startInstall
+SET OUTPUTNAMECLIENT=..\bin\%BUILD_TYPE%\OMLSetupClientx86.msi
+SET OUTPUTNAMESERVER=..\bin\%BUILD_TYPE%\OMLSetupServerx86.msi
+pushd "%~dp0"
+
+REM svn info > info.tmp
+REM for /f "tokens=1,2" %%i in (info.tmp) do if "%%i" == "Revision:" set REVISION_NUMBER=%%j
+REM del info.tmp
+SET WIX_BUILD_LOCATION=%ProgramFilesPath%\Windows Installer XML v3\bin
+SET APP_INTERMEDIATE_PATH=..\obj\%BUILD_TYPE%
+
+REM Cleanup leftover intermediate files
+
+if exist "%APP_INTERMEDIATE_PATH%\*.wixobj" del /f /q "%APP_INTERMEDIATE_PATH%\*.wixobj"
+if exist "%OUTPUTNAMECLIENT%" del /f /q "%OUTPUTNAMECLIENT%"
+if exist "%OUTPUTNAMESERVER%" del /f /q "%OUTPUTNAMESERVER%"
+
+REM Build the MSI for the setup package
+
+"%WIX_BUILD_LOCATION%\candle.exe" -ext WiXNetFxExtension -ext WiXUtilExtension setupclient.wxs setup.wxs -dBuildType=%BUILD_TYPE% -out %APP_INTERMEDIATE_PATH%\
+"%WIX_BUILD_LOCATION%\light.exe" -ext WiXNetFxExtension -ext WixUIExtension -ext WiXUtilExtension "%APP_INTERMEDIATE_PATH%\setupclient.wixobj" "%APP_INTERMEDIATE_PATH%\setup.wixobj" -cultures:en-US -loc setup-en-us.wxl -out "%OUTPUTNAMECLIENT%"
+
+"%WIX_BUILD_LOCATION%\candle.exe" -ext WiXNetFxExtension -ext WiXUtilExtension setupserver.wxs setup.wxs -dBuildType=%BUILD_TYPE% -out %APP_INTERMEDIATE_PATH%\
+"%WIX_BUILD_LOCATION%\light.exe" -ext WiXNetFxExtension -ext WixUIExtension -ext WiXUtilExtension "%APP_INTERMEDIATE_PATH%\setupserver.wixobj" "%APP_INTERMEDIATE_PATH%\setup.wixobj" -cultures:en-US -loc setup-en-us.wxl -out "%OUTPUTNAMESERVER%"
+
+
+
+echo Building Bootstrapper file
+REM Commented out for now
+REM MSBuild omlsetupx86.proj
+
+echo Copying "%OUTPUTNAMECLIENT%" to the Builds folder
+copy "%OUTPUTNAMECLIENT%" ..\..\Builds\X86\
+
+echo Copying "%OUTPUTNAMESERVER%" to the Builds folder
+copy "%OUTPUTNAMESERVER%" ..\..\Builds\X86\
+popd
