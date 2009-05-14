ae5a85fd0e7aedaa17307efcd092b1b0103004fd
mikem2te <mikem2te@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2009-05-14 18:46:44 +0000

    Removed the trim from the strings of the percentage complete calculations. As it was the trim was potentially acting on a null object and stopped colour coding movies in dbeditor on completeness of the movie metadata.

--- OMLEngine/Dao/OMLData.cs
+++ OMLEngine/Dao/OMLData.cs
@@ -17,16 +17,16 @@ namespace OMLEngine.Dao
                 decimal score = 0;
                 decimal possible = 12;
 
-                if (!String.IsNullOrEmpty(Studio.Trim())) score++;
+                if (!String.IsNullOrEmpty(Studio)) score++;
                 if (Runtime > 0) score++;
                 if (ReleaseDate != DateTime.MinValue) score++;
-                if (!String.IsNullOrEmpty(Synopsis.Trim())) score++;
+                if (!String.IsNullOrEmpty(Synopsis)) score++;
                 if (Genres.Count > 0) score++;
                 if (Images.FirstOrDefault(t=> t.ImageType == (byte)ImageType.FrontCoverImage) != null) score++;
-                if (!String.IsNullOrEmpty(AspectRatio.Trim())) score++;
-                if (!String.IsNullOrEmpty(CountryOfOrigin.Trim())) score++;
-                if (!String.IsNullOrEmpty(VideoResolution.Trim())) score++;
-                if (!String.IsNullOrEmpty(VideoStandard.Trim())) score++;
+                if (!String.IsNullOrEmpty(AspectRatio)) score++;
+                if (!String.IsNullOrEmpty(CountryOfOrigin)) score++;
+                if (!String.IsNullOrEmpty(VideoResolution)) score++;
+                if (!String.IsNullOrEmpty(VideoStandard)) score++;
                 if (People.Where(p => p.Role == (byte)PeopleRole.Actor).Count() > 0) score++;
                 if (People.Where(p => p.Role == (byte)PeopleRole.Director).Count() > 0) score++;
 
