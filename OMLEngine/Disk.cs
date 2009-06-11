29b691bb1d3efeda431a107136db7ac9ce3c5939
intemperance <intemperance@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2009-06-11 02:12:54 +0000

    forcing all disks to use a UNC path if they're network drives

--- OMLEngine/Disk.cs
+++ OMLEngine/Disk.cs
@@ -30,8 +30,24 @@ namespace OMLEngine
             get { return _disk.Path; } 
             set 
             {
+                // if it's pointing to a physical drive do the network test
+                if (!string.IsNullOrEmpty(value)
+                    && !value.StartsWith("\\\\") 
+                    && value.Length > 2
+                    && value[1] == ':')
+                {
+                    string uncPath;
+                    
+                    // if it's an attached network drive convert it to a UNC path
+                    if (NetworkScanner.IsNetworkDrive(value[0], out uncPath))
+                    {
+                        value = uncPath + "\\" + value.Remove(0, 2);
+                    }
+                }
+
                 if (!string.IsNullOrEmpty(value) && value.Length > 255)
                     throw new FormatException("Disk path must be 255 characters or less.");
+
                 _disk.Path = value;
                 _diskFeatures = null;
             } 
@@ -203,46 +219,6 @@ namespace OMLEngine
             Format = GetFormatFromPath(Path);
             if (info.MemberCount > 3)
                 ExtraOptions = info.GetString("extraOptions");
-        }
-
-        static string sBasePaths = System.Configuration.ConfigurationSettings.AppSettings["BasePaths"];
-        public void FindPath()
-        {
-            if (Directory.Exists(Path) || File.Exists(Path))
-                return;
-
-            if (sBasePaths == null)
-            {
-                Utilities.DebugLine("Disk.FindPath({0}), not found [BasePaths not set in app.config]", Path);
-                return;
-            }
-
-            string relPath = null;
-            foreach (string basePath in sBasePaths.Split(';'))
-                if (Path.ToLower().StartsWith(basePath.ToLower()))
-                {
-                    relPath = Path.Substring(basePath.Length + 1);
-                    break;
-                }
-
-            if (relPath == null)
-            {
-                Utilities.DebugLine("Disk.FindPath({0}), not found [no BasePaths='{1}' match]", Path, sBasePaths);
-                return;
-            }
-
-            Utilities.DebugLine("Disk.FindPath({0}), relPath = {1}", Path, relPath);
-            foreach (string basePath in sBasePaths.Split(';'))
-            {
-                string newPath = System.IO.Path.Combine(basePath, relPath);
-                if (Directory.Exists(newPath) || File.Exists(newPath))
-                {
-                    Utilities.DebugLine("Disk.FindPath({0}), NewPath = {1}", Path, newPath);
-                    Path = newPath;
-                    return;
-                }
-            }
-            Utilities.DebugLine("Disk.FindPath({0}), no new path match found in BasePaths='{1}'", Path, sBasePaths);
-        }
+        }         
     }
 }
