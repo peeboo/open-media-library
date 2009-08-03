965546109d48c8fb8c095707b899e0bd3620311f
djshultz <djshultz@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2009-08-03 22:24:52 +0000

    Initial support for PFMAPI to be used as an internal ISO mounting library

---
+++ trunk/PFMLib/PFMAPI.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+
+namespace PFMLib
+{
+    public class PFMAPI
+    {
+
+        static bool IsCurrentlyMounted(string fileName)
+        {
+            PfmApi api;
+            if (PfmStatic.ApiFactory(out api) != 0)
+                return false;
+
+            PfmIterator mountList;
+            long changeInstance = 0;
+            long nextChangeInstance = changeInstance;
+            if (api.MountIterate(changeInstance, out nextChangeInstance, out mountList) != 0)
+                return false;
+
+            int mountId = mountList.Next(ref nextChangeInstance);
+            while (mountId != 0)
+            {
+                PfmMount mount;
+                if (api.MountOpenId(mountId, out mount) != 0)
+                {
+                    if (mount.GetFileName().ToLower().CompareTo(fileName.ToLower()) == 0)
+                        return true;
+                }
+                mountId = mountList.Next(ref nextChangeInstance);
+            }
+            return false;
+        }
+
+        static bool IsMountLocationInUse(string driveLetter)
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                if (drive.Name.ToLower().CompareTo(driveLetter.ToLower()) == 0)
+                    return true;
+            }
+            return false;
+        }
+
+        static void Unmount(string fileName)
+        {
+            PfmApi api;
+            if (PfmStatic.ApiFactory(out api) != 0)
+                return;
+
+            PfmMount mount;
+            if (api.MountOpen(fileName, out mount) != 0)
+                return;
+
+            mount.Unmount(0);
+            mount.Dispose();
+        }
+
+        static char? Mount(string fileName)
+        {
+            PfmApi api;
+            if (PfmStatic.ApiFactory(out api) != 0)
+                return null;
+
+            PfmMount mount;
+            if (api.MountOpen(fileName, out mount) != 0)
+                return null;
+
+            char driveLetter = mount.GetDriveLetter();
+            return driveLetter;
+        }
+    }
+}
