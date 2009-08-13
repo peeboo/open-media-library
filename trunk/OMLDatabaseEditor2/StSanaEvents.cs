ad721fd033c2e580f0ccf218015c84717cd406de
djshultz <djshultz@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2009-08-13 09:44:34 +0000

    dammit, forgot these as well

---
+++ trunk/OMLDatabaseEditor2/StSanaEvents.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OMLDatabaseEditor
+{
+    public partial class StSanaEvents : Form
+    {
+        static public void UpdateStatus(string msg)
+        {
+            StSanaEvents.instance.StSanaEventLabel.Text = msg;
+        }
+
+        static private StSanaEvents instance;
+        public StSanaEvents()
+        {
+            StSanaEvents.instance = this;
+            InitializeComponent();
+        }
+    }
+}
