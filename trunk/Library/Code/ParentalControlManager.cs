0e0cf6bd5b0156b82f394aa75b87d3380aaa9772
djshultz <djshultz@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2009-08-15 05:17:14 +0000

    Early pieces of Parental Controls Fixed compile issues the OMLTranslator

---
+++ trunk/Library/Code/ParentalControlManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.MediaCenter;
+using Microsoft.MediaCenter.UI;
+
+namespace Library
+{
+    public class ParentalControlManager : ModelItem
+    {
+        ParentalControls controls;
+        IList<string> ratingsSystems;
+
+        public ParentalControlManager()
+        {
+            controls =
+                OMLApplication.Current.MediaCenterEnvironment.ParentalControls;
+            ratingsSystems = controls.GetAvailableRatingSystems();
+        }
+
+        public void AskForPin()
+        {
+            ParentalPromptCompletedCallback parentalCallback = UnlockParentalLimits;
+            controls.PromptForPin(parentalCallback);
+        }
+
+        public void UnlockParentalLimits(bool correctPinEntered)
+        {
+        }
+    }
+}
