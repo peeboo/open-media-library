4dfb87619d79ec21560420318a9bb18278342f73
djshultz <djshultz@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2009-08-16 00:32:33 +0000

    Lots more Parental coding in place, isLocked will always return false until this is completely ready

--- Library/Code/ParentalControlManager.cs
+++ Library/Code/ParentalControlManager.cs
@@ -3,18 +3,28 @@ using System.Collections.Generic;
 using Microsoft.MediaCenter;
 using Microsoft.MediaCenter.UI;
 
+
+// NOTE: I borrowed much of the logic and some of the code
+// from MediaBrowser on 2009/08/15
+// mediabrowser.googlecode.com
+
 namespace Library
 {
     public class ParentalControlManager : ModelItem
     {
-        Library.Code.V3.MovieItem currentItem;
         ParentalControls controls;
-        IList<string> ratingsSystems;
+        ParentalControlSetting parentalSettings;
+        Timer _timer;
+        DateTime unlockedTime { get; set; }
+        int unlockPeriod { get; set; }
 
         public bool isLocked
         {
             get
             {
+                //if (parentalSettings != null)
+                //    return parentalSettings.Enabled;
+
                 return false;
             }
         }
@@ -23,41 +33,101 @@ namespace Library
         {
             controls =
                 OMLApplication.Current.MediaCenterEnvironment.ParentalControls;
-            ratingsSystems = controls.GetAvailableRatingSystems();
+
+            foreach (string ratingSystem in controls.GetAvailableRatingSystems())
+            {
+                if (ratingSystem.ToUpperInvariant().Contains("movie".ToUpperInvariant()))
+                    parentalSettings = controls[ratingSystem];
+            }
+
+            _timer = new Timer();
+            _timer.Enabled = false;
+            _timer.Interval = 60000;
+            _timer.Tick += new EventHandler(_timer_Tick);
+        }
+
+        void _timer_Tick(object sender, EventArgs e)
+        {
+            if (DateTime.UtcNow >= this.unlockedTime.AddHours(this.unlockPeriod)) this.ReLock();
         }
 
         public void PlayMovie(Library.Code.V3.MovieItem item)
         {
-            this.currentItem = item;
             if (this.isLocked)
             {
-                ParentalPromptCompletedCallback cb = cbPlayMovie;
-                controls.PromptForPin(cb);
+                controls.PromptForPin(delegate(bool goodPin)
+                {
+                    if (goodPin)
+                        item.PlayMovie();
+                });
             } else
                 item.PlayMovie();
         }
 
-        public void cbPlayMovie(bool correctPinEntered)
-        {
-            if (correctPinEntered)
-                this.currentItem.PlayMovie();
-        }
-
         public void PlayAllDisks(Library.Code.V3.MovieItem item)
         {
-            this.currentItem = item;
             if (this.isLocked)
             {
-                ParentalPromptCompletedCallback cb = cbPlayAllDisks;
-                controls.PromptForPin(cb);
+                controls.PromptForPin(delegate(bool goodPin)
+                {
+                    if (goodPin)
+                        item.PlayAllDisks();
+                });
             } else
                 item.PlayAllDisks();
         }
 
-        public void cbPlayAllDisks(bool correctPinEntered)
+        public void UnLock()
         {
-            if (correctPinEntered)
-                this.currentItem.PlayAllDisks();
+            MediaCenterEnvironment env = Microsoft.MediaCenter.Hosting.AddInHost.Current.MediaCenterEnvironment;
+            controls = env.ParentalControls;
+            controls.PromptForPin(delegate(bool goodPin)
+            {
+                if (goodPin)
+                {
+                    this.unlockedTime = DateTime.UtcNow;
+                    this.unlockPeriod = 60000; // currently set to 1 hour
+                    _timer.Start();
+                }
+            });
+        }
+
+        public void ReLock()
+        {
+            _timer.Stop();
         }
     }
 }
+
+//Notes about Parental Ratings research:
+
+//MediaCenterEnvironment.ParentalControls.GetAvailableRatingSystems() // which returns 'EN-US-TV', and 'EN-US-MOVIE'
+
+
+//MediaCenterEnvironment.ParentalControls["EN-US-TV"]
+//MediaCenterEnvironment.ParentalControls["EN-US-MOVIE"]
+
+//.Enabled
+//.BlockUnrated
+//.MaxAllowed
+//.RatingSystem
+
+//TV:
+
+//block all rated content: 0
+//TV-Y: 1
+//TV-Y7: 2
+//TV-G: 3
+//TV-PG: 4
+//TV-14: 5
+//TV-MA: 6
+
+//MOVIE:
+//block all rated movies: 0
+//G: 1
+//PG: 2
+//PG-13: 3
+//R: 4
+//NC-17: 5
+
+//we'll have to come up with our own for adult content
