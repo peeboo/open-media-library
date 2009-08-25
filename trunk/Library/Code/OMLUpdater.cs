e36820651a9ad2365476feb10d8f32698015a992
djshultz <djshultz@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2009-08-25 06:34:03 +0000

    Added FileDownloader to the installers (and the GAC) Rewrote OMLInfo.xml to support udpating of individual components Rewrote OMLUpdater.cs to handle the new xml format and allow for multiple types of updates and their possible required steps

--- trunk/Library/Code/OMLUpdater.cs
+++ trunk/Library/Code/OMLUpdater.cs
@@ -13,6 +13,8 @@ using Microsoft.MediaCenter.Hosting;
 using Microsoft.MediaCenter.UI;
 using System.Reflection;
 using OMLEngine.Settings;
+using FileDownloader;
+using System.Threading;
 
 namespace Library
 {
@@ -46,26 +48,104 @@ namespace Library
                 newVersion = new System.Version(node.Attributes["version"].Value);
                 remoteFile = node.Attributes["url"].Value;
 
-                if (CurrentVersion < newVersion)
-                {
-                    if (OMLApplication.Current.MediaCenterEnvironment.Capabilities.ContainsKey("Console"))
-                    {
+                if (CurrentVersion < newVersion) {
+                    if (OMLApplication.Current.MediaCenterEnvironment.Capabilities.ContainsKey("Console")) {
                         DialogResult reply = OMLApplication.Current.MediaCenterEnvironment.Dialog("A new version of OML is available, would you like to upgrade now?", "Update Available", DialogButtons.Yes | DialogButtons.No, 10, true);
 
                         if (reply == DialogResult.Yes)
                             DownloadUpdate();
-                    }
-                    else
-                    {
+                    } else {
                         // at the extender
                         OMLApplication.Current.MediaCenterEnvironment.Dialog("An update for OML is available, please update from your Media Center PC.", "Update Available", DialogButtons.Ok, 10, true);
                     }
+                } else {
+                    // ok the master version is the same, lets check some of the plugins and extra files
+                    XmlNode downloaderNode = xDoc.SelectSingleNode("/Config/FileDownloader");
+                    ProcessSingleFileUpdate(
+                        new System.Version(downloaderNode.Attributes["version"].Value),
+                        Convert.ToBoolean(downloaderNode.Attributes["GAC"].Value),
+                        downloaderNode.Attributes["type"].Value,
+                        downloaderNode.Attributes["url"].Value,
+                        @"C:\Program Files\OpenMediaLibrary\FileDownloader.exe");
+
+                    XmlNode maximizerNode = xDoc.SelectSingleNode("/Config/Maximizer");
+                    ProcessSingleFileUpdate(
+                        new System.Version(maximizerNode.Attributes["version"].Value),
+                        Convert.ToBoolean(maximizerNode.Attributes["GAC"].Value),
+                        maximizerNode.Attributes["type"].Value,
+                        maximizerNode.Attributes["url"].Value,
+                        @"C:\Program Files\OpenMediaLibrary\Maximizer.exe");
+
+                    XmlNode mencoderNode = xDoc.SelectSingleNode("/Config/MEncoder");
+                    ProcessSingleFileUpdate(
+                        new System.Version(mencoderNode.Attributes["version"].Value),
+                        Convert.ToBoolean(mencoderNode.Attributes["GAC"].Value),
+                        mencoderNode.Attributes["type"].Value,
+                        mencoderNode.Attributes["url"].Value,
+                        @"C:\Program Files\OpenMediaLibrary\Mencoder.exe");
                 }
             }
             catch (Exception)
             {
                 OMLApplication.DebugLine("Update failed");
             }
+        }
+
+        private void ProcessSingleFileUpdate(System.Version newVersion, bool updateGAC, string type, string url, string assemblyFile) {
+            System.Version currentVersion = VersionOfAssembly(assemblyFile);
+            if (currentVersion != null && (currentVersion < newVersion)) {
+                DownloadEngine downloader = new DownloadEngine(url);
+                if (downloader.Download()) {
+                    string newfile = downloader.DownloadedFile;
+                    if (updateGAC) {
+                        RemoveOldGACEntry(assemblyFile);
+                        downloader = null;
+                    }
+
+                    switch (type) {
+                        case "Full":
+                            break;
+                        case "Replace":
+                            File.Move(assemblyFile, assemblyFile + currentVersion);
+                            File.Move(downloader.DownloadedFile, assemblyFile);
+                            break;
+                        case "Restart":
+                            File.Move(assemblyFile, assemblyFile + currentVersion);
+                            File.Move(downloader.DownloadedFile, assemblyFile);
+                            break;
+                        default:
+                            break;
+                    }
+                    if (updateGAC)
+                        AddNewGACEntry(assemblyFile);
+                }
+            }
+        }
+
+        private void RemoveOldGACEntry(string assemblyFile) {
+            Process removeGAC = new Process();
+            removeGAC.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            removeGAC.StartInfo.FileName = @"gacutil.exe";
+            removeGAC.StartInfo.Arguments = string.Format("/u {0}", assemblyFile);
+            removeGAC.Start();
+            Thread.Sleep(new TimeSpan(0, 0, 2)); // sleep for 2 seconds
+        }
+
+        private void AddNewGACEntry(string assemblyFile) {
+            Process startGAC = new Process();
+            startGAC.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startGAC.StartInfo.FileName = @"gacutil.exe";
+            startGAC.StartInfo.Arguments = string.Format("/u {0}", assemblyFile);
+            startGAC.Start();
+            Thread.Sleep(new TimeSpan(0, 0, 2)); // sleep for 2 seconds
+        }
+
+        private System.Version VersionOfAssembly(string path) {
+            if (File.Exists(path)) {
+                FileVersionInfo info = FileVersionInfo.GetVersionInfo(path);
+                return new System.Version(info.ProductVersion);
+            }
+            return null;
         }
 
         void DownloadUpdate()
@@ -147,7 +227,6 @@ namespace Library
             AddInHost context = AddInHost.Current;
             context.ApplicationContext.CloseApplication();
         }
-
 
         string remoteFile;
         string localFile;
