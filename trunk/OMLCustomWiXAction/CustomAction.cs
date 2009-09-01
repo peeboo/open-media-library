b4386c85b13e998633b1d8db88f3650c2cfc052a
djshultz <djshultz@6aa73253-9b4a-0410-aa51-51f77af7c3b9>
2009-09-01 17:55:04 +0000

    More updates to the .net custom action lib Reverted some changes to the wix installers that I didn't mean to commit

--- trunk/OMLCustomWiXAction/CustomAction.cs
+++ trunk/OMLCustomWiXAction/CustomAction.cs
@@ -1,12 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.Management;
 using System.ServiceProcess;
 using Microsoft.Deployment.WindowsInstaller;
 
+using OMLEngine;
+
 namespace OMLCustomWiXAction {
     public class CustomActions {
+        private static StringBuilder prOutput = null;
+        private static StringBuilder prError = null;
+        private string ScriptsPath = string.Empty;
+        private string servername = string.Empty;
+        private string sapassword = string.Empty;
+        private string instancename = string.Empty;
+
         [CustomAction]
         public static ActionResult StartOMLEngineService(Session session) {
             try {
@@ -41,5 +51,164 @@ namespace OMLCustomWiXAction {
         public static ActionResult ValidateDatabase(Session session) {
             return ActionResult.Success;
         }
+
+        #region private methods
+        private bool CheckSQLExists(Session session) {
+            const string instance = "MSSQL$OML";
+            //const string instance = "MSSQLSERVER";
+
+            try {
+                // Enumerate all SQL instances on system
+                ManagementObjectSearcher getAllSQLInstances =
+                    new ManagementObjectSearcher("root\\Microsoft\\SqlServer\\ComputerManagement10",
+                    "select * from SqlServiceAdvancedProperty where SQLServiceType = 1 " +
+                    " and (PropertyName = 'SKUNAME' or PropertyName = 'SPLEVEL')");
+
+                ManagementObjectCollection resultsAll = getAllSQLInstances.Get();
+
+                foreach (ManagementObject service in resultsAll) {
+                    session.Log("[PostInstallerWizard] Emumerating SQL Server Instance : " + service.ToString());
+                }
+
+
+                // Enumerate OML Names instance
+
+                ManagementObjectSearcher getOMLInstance =
+                   new ManagementObjectSearcher("root\\Microsoft\\SqlServer\\ComputerManagement10",
+                   "select * from SqlServiceAdvancedProperty where SQLServiceType = 1 " +
+                   " and ServiceName = '" + instance + "'" +
+                   " and (PropertyName = 'SKUNAME' or PropertyName = 'SPLEVEL')");
+
+                ManagementObjectCollection resultsOML = getOMLInstance.Get();
+
+                // If nothing is returned, SQL isn't installed.
+                if (resultsOML.Count == 0) {
+                    session.Log("[PostInstallerWizard] No OML SQL Servers Not Found");
+                    return false;
+                }
+
+                foreach (ManagementObject service in resultsOML) {
+                    session.Log("[PostInstallerWizard] Found OML SQL Server Instance : " + service.ToString());
+                }
+
+                return true;
+            } catch (ManagementException e) {
+                return false;
+            }
+        }
+
+        private bool RunSQLSetup(Session session) {
+            Process pr = new Process();
+
+            if (File.Exists(session.GetSourcePath("\\SQLInstaller\\SQLEXPR_x86_ENU.exe"))) {
+                pr.StartInfo.FileName = session.GetSourcePath("\\SQLInstaller\\") + "SQLEXPR_x86_ENU.exe";
+                pr.StartInfo.Arguments = "/CONFIGURATIONFILE=\"" + session.GetSourcePath("\\SQLInstaller\\") + "SQLConfigNoTools_x32.ini";
+            } else {
+
+                if (File.Exists(session.GetSourcePath("\\SQLInstaller\\") + "SQLEXPR_x64_ENU.exe")) {
+                    pr.StartInfo.FileName = session.GetSourcePath("\\SQLInstaller\\") + "SQLEXPR_x64_ENU.exe";
+                    pr.StartInfo.Arguments = "/CONFIGURATIONFILE=\"" + session.GetSourcePath("\\SQLInstaller\\SQLConfigNoTools_x64.ini");
+                } else {
+                    session.Log("Cannot find the SQL installers!", "Error");
+                    return false;
+                }
+            }
+
+            // Attempt to capture stdout & stderr, doesn't seem to work but leaving code in
+            // just incase it works but is buggy
+
+            // Set UseShellExecute to false for redirection.
+            pr.StartInfo.UseShellExecute = false;
+
+            // Setup stdout capture
+            pr.StartInfo.RedirectStandardOutput = true;
+            pr.OutputDataReceived += new DataReceivedEventHandler(NetOutputDataHandler);
+            prOutput = new StringBuilder();
+
+            // Setup stderr capture
+            pr.StartInfo.RedirectStandardError = true;
+            pr.ErrorDataReceived += new DataReceivedEventHandler(NetErrorDataHandler);
+            prError = new StringBuilder();
+
+            pr.Start();
+
+            // Start the asynchronous read of the standard output & stderr stream.
+            pr.BeginOutputReadLine();
+            pr.BeginErrorReadLine();
+
+            pr.WaitForExit();
+
+            int ExitCode = pr.ExitCode;
+
+            if (ExitCode == 0)
+                return true;
+            else {
+                session.Log("SQL installer reported error code " + ExitCode.ToString(), "Error");
+                return false;
+            }
+
+        }
+
+        private static void NetOutputDataHandler(object sendingProcess, DataReceivedEventArgs outLine) {
+            // Collect the net view command output.
+            if (!String.IsNullOrEmpty(outLine.Data)) {
+                // Add the text to the collected output.
+                prOutput.Append(Environment.NewLine + "  " + outLine.Data);
+            }
+        }
+
+        private static void NetErrorDataHandler(object sendingProcess, DataReceivedEventArgs errLine) {
+            // Write the error text to the file if there is something
+            // to write and an error file has been specified.
+
+            if (!String.IsNullOrEmpty(errLine.Data)) {
+                prError.Append(Environment.NewLine + "  " + errLine.Data);
+            }
+        }
+
+        private void CheckAndUpgradeSchema(Session session) {
+            OMLEngine.DatabaseManagement.DatabaseManagement dbm = new OMLEngine.DatabaseManagement.DatabaseManagement();
+
+            OMLEngine.DatabaseManagement.DatabaseInformation.SQLState state = dbm.CheckDatabase();
+
+            if (state == OMLEngine.DatabaseManagement.DatabaseInformation.SQLState.OMLDBNotFound) {
+                session.Log("Detected SQL Server but cannot find the database. Click OK to create the database.");
+                // OML Instance but OML database does not exist
+                dbm.ConfigureSQL(ScriptsPath);
+                dbm.UpgradeSchemaVersion(ScriptsPath);
+
+                // Retest the connection
+                state = dbm.CheckDatabase();
+            }
+
+            if (state == OMLEngine.DatabaseManagement.DatabaseInformation.SQLState.OMLDBVersionUpgradeRequired) {
+                session.Log("Detected the OML Database but it requires updating. Click OK to update the database.");
+                dbm.UpgradeSchemaVersion(ScriptsPath);
+
+                // Retest the connection
+                state = dbm.CheckDatabase();
+            }
+
+            if (state == OMLEngine.DatabaseManagement.DatabaseInformation.SQLState.OK) {
+                session.Log("The database appears all fine.");
+                return;
+            }
+        }
+
+        private void ConfigureSQL() {
+            OMLEngine.DatabaseManagement.DatabaseManagement dbm = new OMLEngine.DatabaseManagement.DatabaseManagement();
+            dbm.ConfigureSQL(ScriptsPath);
+        }
+
+        private void WriteSettings() {
+            OMLEngine.DatabaseManagement.DatabaseInformation.DatabaseName = "oml";
+            OMLEngine.DatabaseManagement.DatabaseInformation.OMLUserAcct = "oml";
+            OMLEngine.DatabaseManagement.DatabaseInformation.OMLUserPassword = "oml";
+            OMLEngine.DatabaseManagement.DatabaseInformation.SAPassword = sapassword;
+            OMLEngine.DatabaseManagement.DatabaseInformation.SQLInstanceName = instancename;
+            OMLEngine.DatabaseManagement.DatabaseInformation.SQLServerName = servername;
+            OMLEngine.DatabaseManagement.DatabaseInformation.SaveSettings();
+        }
+        #endregion private methods
     }
 }
